fb3f9d9aba6af5db45d14533bd95e9ba7794ce27
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-02-05 03:06:39 +0530

    by me

--- Pattern Day 1  29 feb/pattern07.cpp
+++ Pattern Day 1  29 feb/pattern07.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-   //inverse pyramid
+//    12345
+//    1234
+//    123
+//    12
+//    1
+for(int i=0;i<5;i++){
+    for(int j=0;j<5-i;j++)
+    cout<<j+1<<" ";
+    cout<<endl;
+}
    
 }
