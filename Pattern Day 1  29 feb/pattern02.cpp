a5727817c04afd4b4f20c34e6ea806c648a6c0ec
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-02-05 01:00:21 +0530

    by harshit

--- Pattern Day 1  29 feb/pattern02.cpp
+++ Pattern Day 1  29 feb/pattern02.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-   for(int i=0;i<4;i++)
+    int n=0;
+    cin>>n;
+
+   for(int i=0;i<n;i++)
    {
-    for(int j=0;j<4;j++)
+    for(int j=0;j<n;j++)
     cout<<"* ";
     cout<<endl;
    }
