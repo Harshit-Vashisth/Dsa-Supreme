3f1bd731bb631fbb55d7903fbd8cf68b7614dce8
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-02-05 01:09:23 +0530

    harshit

--- Pattern Day 1  29 feb/pattern03.cpp
+++ Pattern Day 1  29 feb/pattern03.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    // hollow rectangle
+    for(int i=0;i<3;i++){
+       
+            if(i==0||i==2)
+            {
+                for(int col=0;col<5;col++)
+                cout<<"* ";
+            }
+            else{
+                cout<<"* ";
+                for(int i=0;i<3;i++)
+                cout<<"  ";
+                cout<<"* ";
+            }
+            cout<<endl;
+        
+    }
 }
