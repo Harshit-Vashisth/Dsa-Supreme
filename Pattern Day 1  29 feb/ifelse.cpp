9d13929f08aa719cb9906e05e29fb4d4ffa135b8
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-02-04 18:05:34 +0530

    BY harshit

---
+++ Pattern Day 1  29 feb/ifelse.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int s=0;
+    cin>> s;
+
+    if(s>300)
+    cout<<"India wins";
+    else
+    cout<<"Pakistan wins";
+}
---
+++ Pattern Day 1  29 feb/pattern01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int s=349;
+    if(s>300)
+    cout<<"India wins";
+    else
+    cout<<"Pakistan wins";
+}
