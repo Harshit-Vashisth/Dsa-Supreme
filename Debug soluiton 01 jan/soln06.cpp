655e5040eb6e6e57cd178a920d16672e836af6a0
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-02-07 00:41:05 +0530

    by harshit

--- Debug soluiton 01 jan/soln06.cpp
+++ Debug soluiton 01 jan/soln06.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
-void main(){
+int main(){
     int n;
     cin>>n;
     for(int i=0;i<n;++i){
-        for(int j=j+1;j<=n;j++){
-            if(j==i+1 || j == n - i - 1 || i == 0){
-                cout<<j;
+        for(int j=0;j<n-i;j++){
+            if(i==0|| j ==  n-i-1 || j == 0||i==n-1){
+                cout<<"* ";
             }
             else{
-                cout<<" ";
+                cout<<"  ";
             }
         }
         cout<<endl;
--- Debug soluiton 01 jan/soln07.cpp
+++ Debug soluiton 01 jan/soln07.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 using namespace std;
+int main() {
+  int n;
+  std::cin>>n;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n - i ; j++) {
+      printf("  ");
+    }
+    int ele = i;
+    for (int j = 1; j <=i; j++) {
+      printf("%d ", ele++);
+    }
+    
+    for (int j = ele-2; j >=i ; j--) {
+      printf("%d ", j);
+    }
+    printf("\n");
+  }
+  return 0;
+}
