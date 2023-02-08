e34a304b7249c7c56df82e61a7a9b65553d2310f
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-02-08 19:30:49 +0530

    k

---
+++ pattern Day2  3 feb/alphabetpalindrome.cpp
@@ -0,0 +1,19 @@
+ #include <iostream> 
+using namespace std;
+int main(){
+int n;
+cin>>n;
+for(int i=0;i<n;i++){
+    int j=1;
+    for(;j<i+1;j++){
+        char c='A'+j-1;
+        cout<<c;
+    }
+    for(;j>=1;j--){
+         char c='A'+j-1;
+    cout<<c;
+    }
+    cout<<endl;
+}
+
+}
