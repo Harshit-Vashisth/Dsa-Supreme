54f1567d50c9e7cbb6d81c44ed57dbe4bac1a4e7
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-02-09 00:12:39 +0530

    ok

--- pattern week1/Homework patterns/floydstraiangle.cpp
+++ pattern week1/Homework patterns/floydstraiangle.cpp
@@ -8,7 +8,7 @@ int main(){
     for(int i =0;i<n;i++){
         //star1
         for(int j=0;j<i+1;j++){
-              cout<<c;
+              cout<<c<<" ";
               c++;
         }
         cout<<endl;
