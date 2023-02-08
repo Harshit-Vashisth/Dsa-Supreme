8f2620f3b4f5edcb47dbd50aaa41d0c683287dcb
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-02-08 20:12:50 +0530

    k

--- Homework patterns/numerichollowhalf.cpp
+++ Homework patterns/numerichollowhalf.cpp
@@ -8,9 +8,9 @@ int main(){
         //star1
         for(int j=0;j<i+1;j++){
             if(j==0||i==n-1||j==i||i==0)
-            cout<<j+1;
+            cout<<j+1<<" ";
             else
-            cout<<" ";
+            cout<<"  ";
         }
 
 
