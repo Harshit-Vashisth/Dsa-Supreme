2a921ce760fab5df934cf973aec169ad1175d0e7
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-02-12 23:29:42 +0530

    kk

---
+++ 02 basic of prog week 2/5 feb codes/cound.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void count(int n){
+    for(int i=1;i<=n;i++)
+    cout<<i<<endl;
+    }
+int main(){
+    int a;
+    cin>>a;
+    count(a);
+    return 0;
+}
