1094a9f961448ef000fb66595706d7fe29fd52e8
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-02-13 01:00:59 +0530

    k

---
+++ 03 arrays week3 t&c/10 febarray/arraybasic.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={1,2,3,4,5};
+    for(int i=0;i<5;i++)
+    cout<<arr[i]<<" ";
+    cout<<endl;
+    cout<<arr<<" they are same"<<&arr;
+    return 0;
+} 
