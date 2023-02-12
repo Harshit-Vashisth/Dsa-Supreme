89ef68ed804bf675298d3a6eb82073eecd66b6c3
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-02-13 01:19:00 +0530

    j

---
+++ 03 arrays week3 t&c/10 febarray/5elementdoubles.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //method 1
+    int arr[5];
+    for(int i=0;i<5;i++)
+    cin>>arr[i];
+    
+    for(int i=0;i<5;i++)
+    cout<<arr[i]*2<<" ";
+    
+    //agar array bda h aur values khali h toh remain space me 0 aajate h 
+    
+    cout<<endl;
+
+} 
