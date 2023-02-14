ed3a7c30b057e4415bed18f20251310e37a92775
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-02-14 21:38:01 +0530

    by harshit

---
+++ 03 arrays week3 t&c/11 febarray/fourlet.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<limits>
+#include<vector>
+
+using namespace std;
+
+
+void triplet(vector<int> a,int s){
+    vector<int> ans;
+    for(int i=0;i<a.size()-2;i++){
+        for(int j=i+1;j<a.size()-1;j++)
+        {
+            for(int z=j+1;z<a.size();z++)
+           {
+            for(int k=z+1;k<a.size();k++)
+             if(a[i]+a[j]+a[z]+a[k]==s){
+                cout<<a[i]<<a[j]<<a[z]<<a[k]<<" ";
+           }
+        }
+        }
+    }
+}
+
+int main(){
+int n=0;
+cout<<"Enter the number of elements"<<endl;
+cin>>n;
+    //method 1
+   vector<int> a(n);
+ 
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+   
+    triplet(a,10);
+cout<<endl;
+
+}
---
+++ 03 arrays week3 t&c/11 febarray/sort01.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include<limits>
+#include<vector>
+
+using namespace std;
+vector<int> sorting(vector<int> arr){
+    int s=0,e=arr.size()-1,i=0;
+    while(s<=e){
+        if(arr[i]==0){
+            swap(arr[i],arr[s]);
+            s++;
+            i++;
+        }
+        else
+        {
+            swap(arr[i],arr[e]);
+            e--;
+        }
+    }
+    return arr;
+}
+void print(vector<int> arr){
+    for(auto i : arr)
+    cout<<i<<" ";
+}
+int main(){
+int n=0;
+cout<<"Enter the number of elements"<<endl;
+cin>>n;
+    //method 1
+   vector<int> a(n);
+ 
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+   
+    a=sorting(a);
+cout<<endl;
+print(a);
+
+}
