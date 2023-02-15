f36f9bd06aa4a07cf910d056f55b5ebc2cc768ae
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-02-15 23:59:53 +0530

    By Hashit

---
+++ 03 arrays week3 t&c/12 febarray/coloumnwise.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[3][3];
+
+    //Rowise
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++)
+        cin>>arr[i][j];
+    }
+    cout<<"\n";
+//coloumn wise sum
+int sum=0;
+    for(int i=0;i<3;i++){
+        sum=0;
+        for(int j=0;j<3;j++)
+        sum+=arr[j][i];
+        cout<<sum<<" ";
+    }
+}
