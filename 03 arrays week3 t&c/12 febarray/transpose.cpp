f0ecf78e8b64068b105511e89c59dea042b6bb94
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-02-16 01:49:40 +0530

    by harshit

---
+++ 03 arrays week3 t&c/12 febarray/transpose.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<limits.h>
+using namespace std;
+
+void transpose(int arr[][3])
+{
+    for(int i=0;i<3;i++){
+        for(int j=0;j<i;j++)
+        swap(arr[i][j],arr[j][i]);
+    }
+}
+int main(){
+    int arr[3][3];
+
+    //Rowise
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++)
+        cin>>arr[i][j];
+    }
+    cout<<"\n";
+    transpose(arr);
+     for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++)
+        cout<<arr[i][j];
+        cout<<endl;
+    }
+}
