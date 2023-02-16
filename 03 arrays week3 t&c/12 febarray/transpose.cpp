184ad7cd560ef32dd63a08b23aabc6f6205d5580
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-02-16 23:41:40 +0530

    Update transpose.cpp

--- 03 arrays week3 t&c/12 febarray/transpose.cpp
+++ 03 arrays week3 t&c/12 febarray/transpose.cpp
@@ -17,7 +17,7 @@ int main(){
         for(int j=0;j<3;j++)
         cin>>arr[i][j];
     }
-    cout<<"\n";
+    cout<<"\n\n";
     transpose(arr);
      for(int i=0;i<3;i++){
         for(int j=0;j<3;j++)
