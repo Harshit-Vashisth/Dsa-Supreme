33f73b0fcfaec81aa8737a2b6c1ba66dbafc39ba
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-09 14:46:30 +0530

    By harshit

--- 04 searching and sorting/Homework/exponentialsarch.cpp
+++ 04 searching and sorting/Homework/exponentialsarch.cpp
@@ -3,20 +3,22 @@ using namespace std;
 int binarysearch(vector<int>& arr,int s,int e, int t){
     //
     int mid,ans=0;
-    while(s<e){
+    while(s<=e){
         mid=s+(e-s)/2;
-        if(mid==t)
+        if(arr[mid]==t)
         return mid;
         else if(arr[mid]<t)
         s=mid+1;
         else 
-        e=mid;
+        e=mid-1;
     }
     return -1;
 }
 int search(vector<int> &arr,int n,int t){
-    int i =0;
-    while(arr[i]<=t){
+    if(arr[0]==t)
+    return 0;
+    int i =1;
+    while(i<n&&arr[i]<=t){
         i=i*2;
     }
     return binarysearch(arr,i/2,min(i,n-1),t);
