59f40545cc30d018bb78bde4f97b8d7f254484e0
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-09 14:34:03 +0530

    Create exponentialsarch.cpp

---
+++ 04 searching and sorting/Homework/exponentialsarch.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int search(vector<int> &arr,int n,int t){
+    
+}
+int main(){
+    int t=0;
+    vector<int> v={10,20,30,40,50,60,70,80,83,90};
+    cout<<"Enter the target"<<endl;
+    cin>>t;
+    cout<<"Element is at index "<<search(v,v.size(),t);
+}
