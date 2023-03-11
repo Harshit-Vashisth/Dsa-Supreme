73163eb799b2ac6e2d2320c98bbed3bfd6acb6a0
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-11 23:59:59 +0530

    By harshit

--- 07 recursion week 7/11 march rec 2/arrayprint.cpp
+++ 07 recursion week 7/11 march rec 2/arrayprint.cpp
@@ -1,14 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-void print(int n){
-    if(n==0)
-    return ;
-    print(n-1);
-    cout<<n<<" ";
+void print(vector<int> v,int i,int n){
+    if(i==n)
+    return;
+     cout<<v[i]<<" ";
+    print(v,i+1,n);
+   
 }
 int main(){
     int n;
     cout<<"Enter the number to be printed "<<endl;
     cin>>n;
-   print(n);
+    vector<int> v;
+    while(n--){
+        int h;
+        cin>>h;
+    v.push_back(h);}
+   print(v,0,n);
+   
 }
