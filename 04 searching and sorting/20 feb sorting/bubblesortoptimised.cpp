a083fb9a9c4d22a34d3f382055a882937f3b841e
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-12 02:58:27 +0530

    by harshit

--- 04 searching and sorting/20 feb sorting/bubblesortoptimised.cpp
+++ 04 searching and sorting/20 feb sorting/bubblesortoptimised.cpp
@@ -4,12 +4,16 @@
  int main(){
     vector<int> arr={10,1,7,6,14,9};
     int n=arr.size(); 
-    for(int i=1;i<n;i++){         ///   i=0;i<=n  and j=0 j<n-i-1
+    for(int i=1;i<n;i++){ 
+        bool s=false;        ///   i=0;i<=n  and j=0 j<n-i-1
         for(int j=0;j<n-i;j++) //harr turn me last index sorted hota h na re
         {
-            if(arr[j]>arr[j+1])
+            if(arr[j]>arr[j+1]){
             swap(arr[j],arr[j+1]); 
+            s=true;}
         }
+        if(s==false)
+        break;
     }
      for(auto i:arr) 
     cout<<i<<" ";
