9c793ef14dc848f9566c601a0e82a5ab643c514f
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-12 12:33:03 +0530

    by harshit

---
+++ 07 recursion week 7/12 march rec 3/arraysortornot.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool check(vector<int>& arr,int n ,int i){
+    if(i==n-1)
+    return true;
+    if(arr[i+1]<arr[i])
+    return false;
+    return check(arr,n,i+1);
+}
+int main(){
+    vector<int> v{10,20,30,60,50};
+    int n=5;
+    cout<<"Array is sorted ? "<<check(v,n,0);
+}
