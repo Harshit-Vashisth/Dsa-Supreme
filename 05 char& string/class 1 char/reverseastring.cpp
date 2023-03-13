7640d9523d42a7c746ce7d7c44433ac1fd958c77
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-14 01:58:30 +0530

    by harshit

---
+++ 05 char& string/class 1 char/reverseastring.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void reverse(char name[]){
+    int n=strlen(name);
+    int i=0,j=n-1;
+    while(i<j){
+        swap(name[i],name[j]);
+        i++;j--;
+        }
+}
+int main(){
+    char c[100];
+    cin.getline(c,30);
+    cout<<c;
+    reverse(c);
+    cout<<c;
+
+}
