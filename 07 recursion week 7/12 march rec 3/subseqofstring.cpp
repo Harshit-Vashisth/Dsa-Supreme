f1f6aee7d8ce03e4149be30187bdb7e709ad282f
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-13 21:59:39 +0530

    by harshit

---
+++ 07 recursion week 7/12 march rec 3/subseqofstring.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printseq(string& str,vector<string>& ans,string out,int i){
+    if(i>=str.length())
+    {
+        ans.push_back(out);
+        return;
+    }
+
+    //exclude 
+    printseq(str,ans,out,i+1);
+//include
+    out.push_back(str[i]);
+    printseq(str,ans,out,i+1);
+    
+}
+int main(){
+    cout<<"Enter the string for subseq"<<endl;
+    vector<string> ans;
+    string s;
+    cin>>s;
+    int i=0;
+    string out="";
+    printseq(s,ans,out,i);
+    cout<<"The subsequence of given string is "<<endl;
+    for(auto i:ans)
+    cout<<i<<endl;
+}
