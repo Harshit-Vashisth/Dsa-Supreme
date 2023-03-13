583aa5a6825d1883f07819a2180314a9d115692c
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-14 01:17:42 +0530

    by harshit

--- 05 char& string/class 1 char/charbasic.cpp
+++ 05 char& string/class 1 char/charbasic.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 int main(){
+    //method 1
     char ch[10];
     for(int i=0;i<10;i++)
     cin>>ch[i];
     cout<<ch;
+
+    char name[100];
+    cin>>name;
+    cout<<name;
 }
