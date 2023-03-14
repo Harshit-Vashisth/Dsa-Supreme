b1a9392c0a69a41d76c8ede3a8b409563a873885
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-14 23:45:24 +0530

    by harshit

---
+++ 05 char& string/class 1 char/stringbasic.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cout<<"Enter the string to be check"<<endl;
+    // cin>>s;
+    // cout<<s<<endl;
+    // cout<<"Enter it again"<<endl;
+    getline(cin,s);
+    cout<<s<<endl;
+   
+
+}
