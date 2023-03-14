52755378241e45eb0ce45e6763e664e0f28d2b3b
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-14 16:23:04 +0530

    Create lowertoupper.cpp

---
+++ 05 char& string/class 1 char/lowertoupper.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void toupper(char ch[])
+{
+    for(int i=0;i<strlen(ch);i++){
+        ch[i]=ch[i]-'a'+'A';
+    }
+}
+int main(){
+    char ch[100];
+    cout<<"Enter the string to be check"<<endl;
+    cin.getline(ch,20);
+    toupper(ch);
+     for(int i=0;i<strlen(ch);i++){
+        cout<<ch[i]<<endl;
+    }
+
+}
