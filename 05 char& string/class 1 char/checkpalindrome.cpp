3c129081d99b3ae2dc05d191fba6138926c02eb7
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-14 13:51:33 +0530

    by harshit

---
+++ 05 char& string/class 1 char/checkpalindrome.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool checkpalin(char ch[])
+{
+    int i=0,j=strlen(ch)-1;
+    while(i<=j)
+    {
+        if(ch[i]==ch[j])
+        {
+            i++;j--;
+        }
+        else 
+        return false;
+    }
+    return true;
+}
+int main(){
+    char ch[100];
+    cout<<"Enter the palindrome string to be check"<<endl;
+    cin.getline(ch,20);
+    cout<<checkpalin(ch);
+
+}
