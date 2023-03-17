24668e9edfeabaca42f8bda2bc7ff58088c73f6e
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-17 15:46:10 +0530

    Create removealloccurance.cpp

---
+++ 05 char& string/classs 2 ques/removealloccurance.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int pos=s.find(part);
+        while(pos!=string::npos)
+        {
+            s.erase(pos,part.length());
+            pos=s.find(part);
+        }
+        return s;
+    }
+};
