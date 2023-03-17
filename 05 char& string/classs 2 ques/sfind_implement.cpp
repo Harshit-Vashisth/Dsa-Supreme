9d5c091bf88837865406e7dfa366832a19c7630d
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-17 15:57:00 +0530

    Create sfind_implement.cpp

---
+++ 05 char& string/classs 2 ques/sfind_implement.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+    int findocc(vector<char>& v,int n,vector<char>& part){
+        w
+    }
+int main(){
+    int n=0;
+    vector<char> v={'d','a','a','b','c','b','a','a','b','c','b','c'};
+    vector<char> part={'a','b','c'};
+    
+    int f=findocc(v,n,part);
+}
