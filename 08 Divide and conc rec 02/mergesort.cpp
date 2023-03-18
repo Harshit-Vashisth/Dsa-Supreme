77bea3f0a6b27dc09ca7e4dba85ab8427bc401a5
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-18 23:28:21 +0530

    Update mergesort.cpp

--- 08 Divide and conc rec 02/mergesort.cpp
+++ 08 Divide and conc rec 02/mergesort.cpp
@@ -1,24 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-    void merge(vector<int> v1,vector<int> v2,vector<int>& ans){
-        int len1=v1.size();
-        int len2=v2.size();
-        int i=0,j=0;
-        while(i<len1&&j<len2){
-            if(v1[i]<v2[i])
-            ans.push_back(v1[i++]);
-            else
-            ans.push_back(v2[j++]);
-        }
-        while(i<len1)
-        ans.push_back(v1[i++]);
+    void merge(vector<int>& arr,int s,int mid,int e){
+        
+    }
+    void mergesort(vector<int> &arr,int s,int e){
+        if(s>=e)
+        return;
 
-        while(j<len2)
-        ans.push_back(v2[j++]);
+        int mid=s+(e-s)/2;
+        //left
+        mergesort(arr,s,mid);
+        mergesort(arr,mid+1,e);
+        merge(arr,s,mid,e);
     }
 int main(){
     vector<int> arr={1,4,5,9,10,6,11,3,2};
    
-    mergesort(arr);
+    mergesort(arr,0,arr.size()-1);
    
 }
