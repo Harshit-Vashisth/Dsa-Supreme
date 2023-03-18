a1a2c28ebb2e221201b4f84153eb7d4bd8d97f32
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-18 23:40:02 +0530

    by harshit

--- 08 Divide and conc rec 02/mergesort.cpp
+++ 08 Divide and conc rec 02/mergesort.cpp
@@ -1,8 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-    void merge(vector<int>& arr,int s,int mid,int e){
-        
+    void merge(vector<int>& arr,int s,int e){
+        int mid=s+(e-s)/2;
+        int len1=mid-s+1;
+        int len2=e-mid;
+        vector<int> left;
+        vector<int> right;
+        int k=s;
+        for(int i=0;i<len1;i++){
+            left[i]=arr[k];
+            k++;}
+            k=mid+1;
+            for(int i=0;i<len2;i++){
+            right[i]=arr[k];
+            k++;}
+
+            int i=0,j=0,m=s;
+            while(i<len1&&j<len2){
+                if(left[i]<right[j])
+                    arr[m++]=left[i++];
+                else
+                    arr[m++]=right[j++];
+            }
+            while(i<len1)
+                arr[m++]=left[i++];
+
+        while(j<len2)
+            arr[m++]=right[j++];
     }
+    
     void mergesort(vector<int> &arr,int s,int e){
         if(s>=e)
         return;
@@ -11,11 +37,12 @@ using namespace std;
         //left
         mergesort(arr,s,mid);
         mergesort(arr,mid+1,e);
-        merge(arr,s,mid,e);
+        merge(arr,s,e);
     }
 int main(){
     vector<int> arr={1,4,5,9,10,6,11,3,2};
-   
+   cout<<"Running";
     mergesort(arr,0,arr.size()-1);
-   
+   for(auto i:arr)
+    cout<<i<<" ";
 }
