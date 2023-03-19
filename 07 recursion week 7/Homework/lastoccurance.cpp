87e8298f322f896e40ba79264a607e67e5cd46c2
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-19 23:44:25 +0530

    by harshit

--- 07 recursion week 7/Homework/lastoccurance.cpp
+++ 07 recursion week 7/Homework/lastoccurance.cpp
@@ -1,10 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int lastocc(vector<int> arr, int target){
-    
+int lastocc(vector<int>& arr, int target,int s,int e,int& ans){
+    if(s>=e)
+        return ans;
+    int mid=s+(e-s)/2;
+    if(arr[mid]==target)
+     {
+        ans=mid;
+        s=mid+1;
+     }
+     else if(arr[mid]<target)
+        s=mid+1;
+    else 
+        e=mid-1;
 }
 int main(){
     vector<int> nums={2,2,2,3,3,3,3,4,5,7};
     int t=3;
-    cout<<lastocc(nums,t);
+    int ans=0;
+    cout<<"Last Occurance of an element is :- "<<lastocc(nums,t,0,nums.size(),ans);
 }
