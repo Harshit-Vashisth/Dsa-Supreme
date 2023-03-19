cd69f61d6b1c16bcba199415e5bf2c5721ad9993
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-20 00:29:58 +0530

    by harshit

--- 07 recursion week 7/Homework/addtwostring_IntString.cpp
+++ 07 recursion week 7/Homework/addtwostring_IntString.cpp
@@ -1,26 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
     string addstring(string& s1,string& s2,int i,int j,int& c,string& ans){
-        if(c==0&&i>0&&j>0){
+        if(c==0&&i<0&&j<0){
             reverse(ans.begin(),ans.end());
             return ans;
         }
-        if(i>='0')
-        {
+        if(i>=0){
             c+=s1[i]-'0';
             i--;
         }
-        if(j>='0')
-        {
+        if(j>=0){
             c+=s2[j]-'0';
             j--;
         }
-        ans+=char(c%10-'0');
+        ans+=char(c%10+'0');
         c=c/10;
         return addstring(s1,s2,i,j,c,ans);
     }
 int main(){
     string s1,s2;
+    cout<<"Enter the numbers"<<endl;
     cin>>s1>>s2;
     cout<<"String 1->"<<s1<<endl;
     cout<<"String 2->"<<s2<<endl;
