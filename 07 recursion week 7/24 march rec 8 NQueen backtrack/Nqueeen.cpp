5608aa0bed2db697b42003c48ab944befa6e560c
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-26 16:05:20 +0530

    Update Nqueeen.cpp

--- 07 recursion week 7/24 march rec 8 NQueen backtrack/Nqueeen.cpp
+++ 07 recursion week 7/24 march rec 8 NQueen backtrack/Nqueeen.cpp
@@ -1,7 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-void issafe(vector<vector<int>> board,int row,int col,int n){
-    //left row
+
+bool issafe(vector<vector<int>> board,int row,int col,int n){
+    //left row i j-1
+    int i=row,j=col;
+    while(j>=0){
+        if(board[i][j]==1)
+        return false;
+        j--;
+    }
+     //upper left row i-1 j-1
+    int i=row,j=col;
+    while(j>=0){
+        if(board[i][j]==1)
+        return false;
+        j--;i--;
+    }
+     //bottom left row i+1 j-1
+    int i=row,j=col;
+    while(j>=0){
+        if(board[i][j]==1)
+        return false;
+        j--;i++;
+    }
+    return true;
+}
+void print(vector<vector<int>> board,int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<" "<<board[i][j];
+        }
+        cout<<endl;
+    }
 }
 void solve(vector<vector<int>> board,int col,int n){
     if(col>=n){
