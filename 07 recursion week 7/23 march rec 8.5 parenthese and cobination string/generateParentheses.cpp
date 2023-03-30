28d51f9ed2d93cdd3907728c02d9e340307a82bb
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-30 23:56:37 +0530

    Update generateParentheses.cpp

--- 07 recursion week 7/23 march rec 8.5 parenthese and cobination string/generateParentheses.cpp
+++ 07 recursion week 7/23 march rec 8.5 parenthese and cobination string/generateParentheses.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+void solve(int n,vector<string>& ans,string out,int open,int close){
+    if(open==0 && close==0)
+    {
+        ans.push_back(out);
+        return;
+    }
+    if(i>0&&)
+}
 int main(){
-    
+    int n;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+    vector<string> ans;
+    string out;
+    solve(n,ans,out,n,n);
+    for(auto i: ans)
+    cout<<i<<" ";
+
 }
