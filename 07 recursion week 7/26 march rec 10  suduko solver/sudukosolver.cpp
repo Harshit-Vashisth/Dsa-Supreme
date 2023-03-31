3003abd8019f94a7d23531a048342862f57b1d93
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-31 22:36:29 +0530

    by harshit

--- 07 recursion week 7/26 march rec 10  suduko solver/sudukosolver.cpp
+++ 07 recursion week 7/26 march rec 10  suduko solver/sudukosolver.cpp
@@ -1,26 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool issafe(int val,int board[][9],int curr_row,int curr_col){
+bool issafe(int val,int board[][9],int row,int col){
     //row
-    for(int col=0;col<9;col++){
-        if(board[curr_row][col]==val)
+    for(int i=0;i<9;i++)
+        if((board[row][i]==val)||(board[i][col]==val)||(board[3*+(row/3)+(i/3)][3*(col/3)+(i%3)])==val)
         return false;
-    }
-    //col
-    for(int row=0;row<9;row++){
-            if(board[row][curr_col]==val)
-            return false;
-    }
-    //3*3 board
-    //we can use maps
-    for(int i=0;i<9;i++){
-        if(board[3*(curr_row/3)+(i/3)][3*(curr_col/3)+(i%3)]==val)
-        return false;
-    }
+
     return true;
 }
 //return true or false base on soln found or not
-bool solve(int board[9][9],int n){
+bool solve(int board[][9]){
+    int n=9;
     //empty cell ko fill
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
@@ -33,7 +23,7 @@ bool solve(int board[9][9],int n){
                         //insert
                         board[i][j]=val;
                         //bali recursion dekh lega
-                        bool aage=solve(board,n);
+                        bool aage=solve(board);
                         if(aage==true)
                            return true;
                         board[i][j]=0;
@@ -56,9 +46,9 @@ int board[9][9]={
             {0,0,0,0,0,0,4,7,6},
             {0,7,0,0,4,5,0,0,0},
             {0,0,8,0,0,9,0,0,0}
-};
+            };
 int n=9;
-solve(board,9);
+solve(board);
 for(int i=0;i<9;i++){
     for(int j=0;j<9;j++)
     cout<<board[i][j]<<" ";
