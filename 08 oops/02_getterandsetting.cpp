04ad75ac12388dcec454a3013dfb78302bd0d40e
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-07 15:58:56 +0530

    Update 02_getterandsetting.cpp

--- 08 oops/02_getterandsetting.cpp
+++ 08 oops/02_getterandsetting.cpp
@@ -2,16 +2,25 @@
 using namespace std;
 
 class Animal{
-    public:
-    int age;
+    private:
+    int age,weight;
     string name;
+    public:
     void eat(){
         cout<<"eating"<<endl;
     }
     void sleep(){
         cout<<"Sleep"<<endl;
     }
-}; 
+    int getter()
+    {
+        return weight;
+    }
+    void setter(int weight)
+    {
+        this->weight=weight;
+    }
+    }; 
 int main(){
     cout<<"Size of empty class is "<< sizeof(Animal) <<endl;  // epmty -> 1 byte  filled=size of input
 
@@ -19,9 +28,7 @@ int main(){
     //object creation 
 //static
 Animal r;  //animal type ka object
-r.age=12;
-    cout<<"Age of ramesh is"<<r.age<<endl;
-    r.name="Lion";
-    cout<<"Name of ramesh is"<<r.name<<endl;
-    return 0;
-}
+ r.setter(3);
+ cout<<r.getter()<<endl;
+return 0;
+}    
