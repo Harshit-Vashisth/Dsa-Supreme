2bbb128e5618d115c8ebb83623299dc04c1573b9
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-07 18:01:59 +0530

    Create parameterisecons.cpp

---
+++ 08 oops/parameterisecons.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Animal{
+
+    
+    private:
+    int age,weight;
+    string name;
+    public:
+    int type;
+    void eat(){
+        cout<<"eating"<<endl;
+    }
+    void sleep(){
+        cout<<"Sleep"<<endl;
+    }
+    int getter()
+    {
+        return weight;
+    }
+    void setter(int weight)
+    {
+        this->weight=weight;this is a pointer to curremt object
+    }
+    }; 
+int main(){
+    cout<<"Size of empty class is "<< sizeof(Animal) <<endl;  // epmty -> 1 byte  filled=size of input
+
+
+    //object creation 
+//static
+Animal* r=new Animal();  //animal type ka  dynamically
+r->type=15;   //-> arrow -> is the easist way to access  them     
+ (*r).setter(3);
+ cout<<(*r).getter()<<endl;
+ delete r;
+return 0;
+}    
