397c44c46f3860bcb455e807dba83902cc2147f8
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-07 15:13:32 +0530

    by harshit

--- 08 oops/01_oopsbasic.cpp
+++ 08 oops/01_oopsbasic.cpp
@@ -2,9 +2,25 @@
 using namespace std;
 
 class Animal{
+    public:
+    int age;
+    string name;
+    void eat(){
 
+
+    }
+    void sleep(){
+
+    }
 }; 
 int main(){
-    cout<<"Size of empty class is "<< sizeof(Animal) <<endl;
+    cout<<"Size of empty class is "<< sizeof(Animal) <<endl;  // epmty -> 1 byte  filled=size of input
+
+
+    //object creation 
+//static
+Animal r;  //animal type ka object
+    cout<<"Age of ramesh is"<<r.age;
+    cout<<"Age of ramesh is"<<r.name;
     return 0;
 }
