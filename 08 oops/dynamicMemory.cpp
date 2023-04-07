f81357f8df4bb129327c6457a51dc3a68d3c5ab9
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-07 16:14:58 +0530

    Update dynamicMemory.cpp

--- 08 oops/dynamicMemory.cpp
+++ 08 oops/dynamicMemory.cpp
@@ -1,1 +1,34 @@
 #include<bits/stdc++.h>
+using namespace std;
+
+class Animal{
+    private:
+    int age,weight;
+    string name;
+    public:
+    void eat(){
+        cout<<"eating"<<endl;
+    }
+    void sleep(){
+        cout<<"Sleep"<<endl;
+    }
+    int getter()
+    {
+        return weight;
+    }
+    void setter(int weight)
+    {
+        this->weight=weight;
+    }
+    }; 
+int main(){
+    cout<<"Size of empty class is "<< sizeof(Animal) <<endl;  // epmty -> 1 byte  filled=size of input
+
+
+    //object creation 
+//static
+Animal r;  //animal type ka object
+ r.setter(3);
+ cout<<r.getter()<<endl;
+return 0;
+}    
