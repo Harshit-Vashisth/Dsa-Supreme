b148ad0dd63347df384b58558e35ac79dcee9e15
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-09 17:53:43 +0530

    by harshti

--- 08 oops/02 oops/hybridinherit.cpp
+++ 08 oops/02 oops/hybridinherit.cpp
@@ -1,30 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//here we are going to perform encapsulation 
-class A{
+class parent {
+    public:
+    void printing(){
+        cout<<"\nparent"<<endl;
+    }
+};
+class A: public parent{
     public:
     int age,wt;
     void eat(){
         cout<<"eating"<<endl;
     }
 };
-class B:public A{
+class B:public parent{
     public : 
     int g=10;
     void dance(){
         cout<<"Dancing"<<endl;
     }
 };
-class C: public B{
+class C: public B,public A{
     public:
     int d=0;
 
 };
+class D: public C{
+    public:
+    int d2=0;
+
+};
 int main(){
-    C* o=new C();
+    D* o=new D();
     o->eat();
     o->dance();
     cout<<o->g;
     cout<<o->d;
+    o->A::printing();
 }
