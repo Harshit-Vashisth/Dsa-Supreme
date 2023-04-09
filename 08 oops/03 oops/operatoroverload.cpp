02907dc213cf05825db2ff3f4ab08121f9d18fe9
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-10 01:19:13 +0530

    by harshit

--- 08 oops/03 oops/operatoroverload.cpp
+++ 08 oops/03 oops/operatoroverload.cpp
@@ -6,12 +6,12 @@ class para{
     int val;
     void operator+(para& o2){
         int val1=this->val;
-        int val2=this->val;
+        int val2=obj->val;
         cout<<val1-val2;
     }
 };
 int main(){
-    para* o1,o2;
+    para o1,o2;
     o1.val=20;
     o2.val=10;
     o1+o2;
