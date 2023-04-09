ccb47011c5291fa480ed0462cbbb2a3f50787883
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-09 17:46:39 +0530

    by harshit

---
+++ 08 oops/02 oops/multipleinheri.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//here we are going to perform encapsulation 
+class A{
+    public:
+    int age,wt;
+    void eat(){
+        cout<<"eating"<<endl;
+    }
+};
+class B{
+    public : 
+    int g=10;
+    void dance(){
+        cout<<"Dancing"<<endl;
+    }
+};
+class C: public B,public A{
+    public:
+    int d=0;
+
+};
+int main(){
+    C* o=new C();
+    o->eat();
+    o->dance();
+    cout<<o->g;
+    cout<<o->d;
+}
