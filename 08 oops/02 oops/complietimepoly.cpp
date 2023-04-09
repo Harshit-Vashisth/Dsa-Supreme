97d87cc9b9d5a322d94528fce099f1c8575f59b9
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-10 00:33:47 +0530

    by harshit

--- 08 oops/02 oops/complietimepoly.cpp
+++ 08 oops/02 oops/complietimepoly.cpp
@@ -1,3 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Math{
+    public:
+    int sum(int a,int b){
+        return a+b;
+    }
+    int sum(int a,int b,int c){
+        return a+b+c;
+    }
+    int sum(int a, float b){
+        return a+b;
+    }
+    int sum(int a, double b){
+        return a+b;
+    }
+};
+int main(){
+    Math* m=new Math();
+    cout<<m->sum(4,5)<<endl;
+    cout<<m->sum(4,5,8)<<endl;
+    cout<<m->sum(4,4.3f)<<endl;
+    cout<<m->sum(4,5.23)<<endl;
+
+}
