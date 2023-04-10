d1998641df6d75f97ef5a367a8f3420c83b32f2b
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-11 02:46:51 +0530

    by harshit

--- 08 oops/03 oops/overriddingbase.cpp
+++ 08 oops/03 oops/overriddingbase.cpp
@@ -11,7 +11,7 @@ class A{
 
 class B : public A{
     public:
-    void print()
+    virtual void print()
         {
             cout<<"Inside B"<<endl;
         }
@@ -20,6 +20,12 @@ class B : public A{
 int main(){
     B* o=new B();
     o->print();
+    A* o1=new A();
+    o1->print();
 
-   
+    A* o2=new B();  //this is called upcating
+    o2->print();
+
+    A* o3=new B();  //now putting virtual
+    o3->print();
 }
