383de5ee42451e190cb5019ed95361096179a2cf
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-10 23:57:51 +0530

    Update tableofpublicpvt.cpp

--- 08 oops/02 oops/tableofpublicpvt.cpp
+++ 08 oops/02 oops/tableofpublicpvt.cpp
@@ -1,18 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 class A{
-    private:
-    int a=10;
     public:
     int b=20;
     protected:
     int c=30;
+    private:
+    int a=10;
 };
 
 class B:public A{
 
 };
 
-class B : public B{
+class C : protected A{
 
 };
+
+class D : private A{
+
+};
+int main(){
+    B* o=new B();
+    cout<<o->b;
+    // cout<<o->c;
+    // cout<<o->a;   A AND C ARE PROTECTED AND PVT the protected scope in only there where it is extended
+
+}
