e8d12e455311bf4213c1ec659cedde518fc40e70
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-12 00:31:20 +0530

    harshit

--- 09 Linked List/01 linkedlist/singlylinkedlist.cpp
+++ 09 Linked List/01 linkedlist/singlylinkedlist.cpp
@@ -1,1 +1,48 @@
 #include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(){
+        val=0;
+        next=NULL;
+    }
+    Node(int val){
+        this->val=val;
+        next=NULL;
+    }
+};
+
+void print(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->val<<" -> ";
+        temp=temp->next;
+    }
+}
+void insertAthead(Node* &head,int data){
+    Node* newnode=new Node();
+    if(head==NULL){
+    head=newnode;
+    return;}
+
+    newnode->next=head;
+    head=newnode;
+}
+int main(){
+    Node* head=new Node(30);
+    Node* a=new Node(40);
+    Node* b=new Node(50);
+    Node* c=new Node(60);
+    Node* d=new Node(70);
+    Node* e=new Node(80);
+    head->next=a;
+    a->next=b;
+    b->next=c;
+    c->next=d;
+    d->next=e;
+    print(head);
+    insertAthead(head,20);
+    print(head);
+}
