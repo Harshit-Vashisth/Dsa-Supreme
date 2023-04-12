5a149627a42b82790a440a82fc4078ded8f35507
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-12 20:21:32 +0530

    Update doublylinkedlist2.cpp

---
+++ 09 Linked List/02 linkedlist/doublylinkedlist2.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node* prev;
+    Node(){
+        val=0;
+        next=NULL;
+        prev=NULL;
+    }
+    Node(int val){
+        this->val=val;
+        prev=NULL;
+        next=NULL;
+    }
+};
+
+void print(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->val<<" -> ";
+        temp=temp->next;
+    }
+}
+
+int main(){
+    Node* head=new Node(10);
+    Node* a=new Node(20);
+    Node* b=new Node(30);
+    Node* c=new Node(40);
+    Node* d=new Node(50);
+    
+    head->next=a;
+    a->prev=head;
+
+    a->next=b;
+    b->prev=a;
+
+    b->next=c;
+    c->prev=b;
+
+    c->next=d;
+    d->prev=c;
+    
+    print(head);
+}
+    
+    
