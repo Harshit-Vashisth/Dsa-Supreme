e889d43f92c9445beb3ad1b8f72ffa554cd072cd
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-12 19:54:25 +0530

    by harshit

--- 09 Linked List/01 linkedlist/singlulinkedlist2.cpp
+++ 09 Linked List/01 linkedlist/singlulinkedlist2.cpp
@@ -41,6 +41,81 @@ void insertAttail(Node* &head,Node* &tail,int val){
     tail->next=newnode;
     tail=newnode;
 }
+int length(Node* head){
+    Node* temp=head;
+    int len=0;
+    while(temp!=NULL)
+    {
+        temp=temp->next;
+        len++;
+    }
+    return len;
+
+}
+void insertAtpos(Node* &head,Node* &tail,int pos,int val){
+    Node* newnode=new Node(val);
+    if(head==NULL){
+        head=newnode;
+        tail=newnode;
+    return ;
+    }
+    if(pos==1){
+        insertAthead(head,tail,val);
+        return;
+    }
+    int len=length(head);
+    if(pos>=len-1){
+        insertAttail(head,tail,val);
+        return;
+    }
+    Node* prev=head;
+    int i=1;
+    while(i<pos-1){
+        prev=prev->next;
+        i++;
+    }
+    Node* curr=prev->next;
+    prev->next=newnode;
+    newnode->next=curr->next;
+}
+void deletenode(Node* &head,Node* &tail,int pos){
+    if(head==NULL){
+        cout<<"Deletion not possible";
+        return;}
+    if(pos==1)
+    {
+        Node* temp=head;
+        head=head->next;
+        temp->next=NULL;
+        delete temp;
+    }
+    int len=length(head);
+    if(pos==len)
+    {
+        int i=1;
+        Node* prev=head;
+        while(i<pos-1){
+            prev=prev->next;
+            i++;
+        }
+        prev->next=NULL;
+        Node* temp=tail;
+        tail=prev;
+        delete temp;
+        return ;
+    }
+    Node* prev=head;
+    int i=1;
+    while(i<pos-1)
+    {
+        prev=prev->next;
+        i++;
+    }
+    Node* curr=prev->next;
+    prev->next=curr->next;
+    curr->next=NULL;
+    delete curr;
+}
 int main(){
     Node* head=NULL;
     Node* tail=NULL;    
@@ -54,6 +129,28 @@ int main(){
     insertAttail(head,tail,50);
     insertAttail(head,tail,60);
     insertAttail(head,tail,100);
+    cout<<endl;
+    print(head);
+    cout<<endl;
+
+    cout<<length(head);
+
+    insertAtpos(head,tail,4,500);
+    cout<<endl;
+    print(head);
+    insertAtpos(head,tail,1,500);
+    cout<<endl;
+    print(head);
+    insertAtpos(head,tail,10,500);
+    cout<<endl;
+    print(head);
+    deletenode(head,tail,1);
+    cout<<endl;
+    print(head);
+    deletenode(head,tail,4);
+    cout<<endl;
+    print(head);
+    deletenode(head,tail,6);
     cout<<endl;
     print(head);
 }
