97b9201409aaead97fb8bf33da8f57a5bf917bfd
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-19 02:06:19 +0530

    Update clirculardoublylinkedlist.cpp

--- 09 Linked List/02 linkedlist/clirculardoublylinkedlist.cpp
+++ 09 Linked List/02 linkedlist/clirculardoublylinkedlist.cpp
@@ -45,11 +45,50 @@ void insertAthead(Node* &head,int val){
     head->prev=newnode;
     head=newnode;
 }
+
+void insertAtend(Node* &head,int val){
+    Node* newnode=new Node(val);
+    if(head==NULL){
+        head=newnode;
+        head->next=head;
+        head->prev=head;
+        return;
+    }
+    Node* temp=head;
+    while(temp->next!=head){
+        temp=temp->next;
+    }
+    temp->next=newnode;
+    newnode->prev=temp;
+    newnode->next=head;
+    head->prev=newnode;
+}
+
+int length(Node* head){
+    int i =0;
+    Node* temp=head;
+    while(temp->next!=head){
+        temp=temp->next;
+        i++;
+    }
+}
+
 void insertAtpos(Node* &head,int pos,int val){
     Node* newnode=new Node();
     if(head==NULL){
         head=newnode;
         head->next=head;
         head->prev=head;
+        return;
     }
+    if(pos==1)
+    {
+        insertAthead(head,val);
+        return;
+    }
+    int len=length(head);
+    if(pos==len){
+        insertAtend(head,val);
+    }
+
 }
