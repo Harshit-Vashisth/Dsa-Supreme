c158d0db35b1cc6525eb1d0be90cab1b82c9d598
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-19 00:43:58 +0530

    Update clirculardoublylinkedlist.cpp

--- 09 Linked List/02 linkedlist/clirculardoublylinkedlist.cpp
+++ 09 Linked List/02 linkedlist/clirculardoublylinkedlist.cpp
@@ -3,15 +3,26 @@ using namespace std;
 
 class Node{
     public:
-    int data;
+    int val;
     Node* next;
     Node* prev;
     Node(){
-        data=0;
+        val=0;
         next=NULL;
         prev=NULL;
     }
-    Node(int data){
-        this->data=data;
+    Node(int val){
+        this->val=val;
+        next=NULL;
+        prev=NULL;
     }
+};
+
+void print(Node* head){
+    Node* temp=head;
+    do{
+        cout<<temp->val<<" -> ";
+        temp=temp->next;
+    }while(temp!=head);
 }
+
