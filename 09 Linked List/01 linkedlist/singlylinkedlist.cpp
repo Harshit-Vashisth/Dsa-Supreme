f0e0eed3aa960fb1481029c2827f33e799b1b065
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-05-20 15:18:07 +0530

    by harshit

--- 09 Linked List/01 linkedlist/singlylinkedlist.cpp
+++ 09 Linked List/01 linkedlist/singlylinkedlist.cpp
@@ -2,24 +2,56 @@
 using namespace std;
 
 class Node{
+    public:
     int data;
     Node* next;
-    public:
+    
     Node(){
         data=0;
-        next=NULL;
+        this->next=NULL;
     }
     Node(int data){
         this->data=data;
-        next=NULL;
+        this->next=NULL;
     }
 };
 void insertatHead(Node* &head,Node* &tail,int val){
-    Node* newnode=new
+    Node* newnode=new Node(val);
     if(head==NULL){
+        head=newnode;
+        tail=newnode;
+        return;
+    }
+    newnode->next=head;
+    head=newnode;
+}
+void insertattail(Node* &head,Node* &tail,int val){
+    Node* newnode=new Node(val);
+    if(head==NULL){
+        head=newnode;
+        tail=newnode;
+        return;
+    }
+    tail->next=newnode;
+    tail=newnode;
+}
+void print(Node* head){
+    if(head==NULL)
+    cout<<"linkedlist is empty "<<endl;
+    while(head!=NULL)
+    {
+        cout<<head->data<<"-> ";
+        head=head->next;
     }
 }
 int main(){
-    Node* head;
-
+    Node* head=new Node();
+    Node* tail=new Node();
+    insertatHead(head,tail,30);
+    insertatHead(head,tail,20);
+    insertatHead(head,tail,10);
+    insertattail(head,tail,40);
+    insertattail(head,tail,50);
+    insertattail(head,tail,60);
+    print(head);
 }
