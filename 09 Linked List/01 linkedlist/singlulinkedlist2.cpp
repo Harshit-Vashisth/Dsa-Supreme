dba7d101cfcc3c73b309cacfeddba69eac48eeac
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-05-20 23:44:05 +0530

    by harshit

--- 09 Linked List/01 linkedlist/singlulinkedlist2.cpp
+++ 09 Linked List/01 linkedlist/singlulinkedlist2.cpp
@@ -45,6 +45,76 @@ void print(Node* head){
         temp=temp->next;
     }
 }
+int length(Node* head){
+    Node* temp=head;
+    int i =0;
+    while(temp!=NULL){
+        i++;
+        temp=temp->next;
+    }
+    return i;
+}
+void insertatpos(Node* &head,Node* &tail,int pos,int val){
+    Node* newnode=new Node(val);
+    if(head==NULL){
+        head=newnode;
+        tail=newnode;
+        return; 
+    }
+    if(pos==1)
+    {
+        insertatHead(head,tail,val);
+        return;
+    }
+    int len=length(head);
+    if(pos>=len-1)
+    {
+        insertattail(head,tail,val);
+        return;
+    }
+    int i =1;
+    Node* prev=head;
+    while(i<pos-1){
+        prev=prev->next;
+        i++;
+    }
+    Node* curr=prev->next;
+    prev->next=newnode;
+    newnode->next=curr;
+}
+void deletepos(Node* &head,Node* &tail,int pos){
+    Node* temp=head;
+    if(head==NULL)
+    return ;
+    if(pos==1){
+        Node* temp=head;
+        head=head->next;
+        temp->next=NULL;
+        delete temp;
+        return;
+    }
+    int len=length(head);
+    if(pos==len){
+        Node* prev=head;
+        while(prev->next!=tail){
+            prev=prev->next;
+        }
+        prev->next=NULL;
+        Node* temp=tail;
+        delete temp;
+        tail=prev;
+        return;
+    }
+    int i =1;
+    Node* prev=head;
+    while(i<pos-1){
+        prev=prev->next;i++;
+    }
+    Node* curr=prev->next;
+    prev->next=curr->next;
+    curr->next=NULL;
+    delete curr;
+}
 int main(){
     Node* head=NULL;
     Node* tail=NULL;
@@ -54,5 +124,19 @@ int main(){
     insertattail(head,tail,40);
     insertattail(head,tail,50);
     insertattail(head,tail,60);
+    insertatpos(head,tail,4,70);
+    insertatpos(head,tail,2,90);
+    print(head);
+    deletepos(head,tail,1);
+    cout<<endl;
+    print(head);
+    deletepos(head,tail,5);
+    cout<<endl;
+    print(head);
+    deletepos(head,tail,4);
+    cout<<endl;
+    print(head);
+    deletepos(head,tail,4);
+    cout<<endl;
     print(head);
 }
