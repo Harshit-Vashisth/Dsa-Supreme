52cf0d43007f3ece1f222dea0a080801a1a13e9c
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-05-20 14:44:00 +0530

    Update singlylinkedlist.cpp

--- 09 Linked List/01 linkedlist/singlylinkedlist.cpp
+++ 09 Linked List/01 linkedlist/singlylinkedlist.cpp
@@ -1,1 +1,25 @@
-#include
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    int data;
+    Node* next;
+    public:
+    Node(){
+        data=0;
+        next=NULL;
+    }
+    Node(int data){
+        this->data=data;
+        next=NULL;
+    }
+};
+void insertatHead(Node* &head,Node* &tail,int val){
+    Node* newnode=new
+    if(head==NULL){
+    }
+}
+int main(){
+    Node* head;
+
+}
