64ad95806e9cfac5c96e26a60d01fa2cf46f730a
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-05-21 17:33:40 +0530

    Update reversesinglyLL.cpp

--- 09 Linked List/01 linkedlist/reversesinglyLL.cpp
+++ 09 Linked List/01 linkedlist/reversesinglyLL.cpp
@@ -1,79 +1,75 @@
-
-    #include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 class Node{
     public:
-    int data;
+    int val;
     Node* next;
-    
     Node(){
-        data=0;
-        this->next=NULL;
+        val=0;
+        next=NULL;
     }
-    Node(int data){
-        this->data=data;
-        this->next=NULL;
+    Node(int val){
+        this->val=val;
+        next=NULL;
     }
 };
-void insertatHead(Node* &head,Node* &tail,int val){
+
+void print(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->val<<" -> ";
+        temp=temp->next;
+    }
+}
+void insertAthead(Node* &head,Node* &tail,int val){
     Node* newnode=new Node(val);
     if(head==NULL){
-        head=newnode;
-        tail=newnode;
-        return;
-    }
+    head=newnode;
+    tail=newnode;
+    return;}
+
     newnode->next=head;
     head=newnode;
 }
-void insertattail(Node* &head,Node* &tail,int val){
-    Node* newnode=new Node(val);
+void insertAttail(Node* &head,Node* &tail,int val){
+     Node* newnode=new Node(val);
     if(head==NULL){
-        head=newnode;
-        //tail=newnode;
-        return;
-    }
+    head=newnode;
+    //tail=newnode;
+    return;}
+
     tail->next=newnode;
     tail=newnode;
 }
-void print(Node* head){
-    Node* temp=head;
-   
-    while(temp->next!=NULL)
-    {
-        cout<<temp->data<<"-> ";
-        temp=temp->next;
-    }
-}
 int length(Node* head){
     Node* temp=head;
-    int i =0;
-    while(temp!=NULL){
-        i++;
+    int len=0;
+    while(temp!=NULL)
+    {
         temp=temp->next;
+        len++;
     }
-    return i;
+    return len;
+
 }
-void insertatpos(Node* &head,Node* &tail,int pos,int val){
+void insertAtpos(Node* &head,Node* &tail,int pos,int val){
     Node* newnode=new Node(val);
     if(head==NULL){
         head=newnode;
         tail=newnode;
-        return; 
+    return ;
     }
-    if(pos==1)
-    {
-        insertatHead(head,tail,val);
+    if(pos==1){
+        insertAthead(head,tail,val);
         return;
     }
     int len=length(head);
-    if(pos>=len-1)
-    {
-        insertattail(head,tail,val);
+    if(pos>=len-1){
+        insertAttail(head,tail,val);
         return;
     }
-    int i =1;
     Node* prev=head;
+    int i=1;
     while(i<pos-1){
         prev=prev->next;
         i++;
@@ -82,61 +78,92 @@ void insertatpos(Node* &head,Node* &tail,int pos,int val){
     prev->next=newnode;
     newnode->next=curr;
 }
-void deletepos(Node* &head,Node* &tail,int pos){
-    Node* temp=head;
-    if(head==NULL)
-    return ;
-    if(pos==1){
+void deletenode(Node* &head,Node* &tail,int pos){
+    if(head==NULL){
+        cout<<"Deletion not possible";
+        return;}
+    if(pos==1)
+    {
         Node* temp=head;
         head=head->next;
         temp->next=NULL;
         delete temp;
-        return;
     }
     int len=length(head);
-    if(pos==len){
+    if(pos==len)
+    {
+        int i=1;
         Node* prev=head;
-        while(prev->next!=tail){
+        while(i<pos-1){
             prev=prev->next;
+            i++;
         }
         prev->next=NULL;
         Node* temp=tail;
-        delete temp;
         tail=prev;
-        return;
+        delete temp;
+        return ;
     }
-    int i =1;
     Node* prev=head;
-    while(i<pos-1){
-        prev=prev->next;i++;
+    int i=1;
+    while(i<pos-1)
+    {
+        prev=prev->next;
+        i++;
     }
     Node* curr=prev->next;
     prev->next=curr->next;
     curr->next=NULL;
     delete curr;
 }
+Node* reverse(Node* &prev,Node* &curr){
+    if(curr==NULL){
+        return prev;
+    }
+    Node* next=curr->next;
+    curr->next=prev;
+    reverse(curr,next);
+}
 int main(){
     Node* head=NULL;
-    Node* tail=NULL;
-    insertatHead(head,tail,30);
-    insertatHead(head,tail,20);
-    insertatHead(head,tail,10);
-    insertattail(head,tail,40);
-    insertattail(head,tail,50);
-    insertattail(head,tail,60);
-    insertatpos(head,tail,4,70);
-    insertatpos(head,tail,2,90);
-    print(head);
-    deletepos(head,tail,1);
+    Node* tail=NULL;    
+   
+    insertAthead(head,tail,30);
+    insertAthead(head,tail,20);
+    insertAthead(head,tail,10);
     cout<<endl;
     print(head);
-    deletepos(head,tail,5);
+    insertAttail(head,tail,40);
+    insertAttail(head,tail,50);
+    insertAttail(head,tail,60);
+    insertAttail(head,tail,100);
     cout<<endl;
     print(head);
-    deletepos(head,tail,4);
+    cout<<endl;
+
+    cout<<length(head);
+
+    insertAtpos(head,tail,4,500);
     cout<<endl;
     print(head);
-    deletepos(head,tail,4);
+    insertAtpos(head,tail,1,500);
+    cout<<endl;
+    print(head);
+    insertAtpos(head,tail,10,500);
+    cout<<endl;
+    print(head);
+    deletenode(head,tail,1);
+    cout<<endl;
+    print(head);
+    deletenode(head,tail,4);
+    cout<<endl;
+    print(head);
+    deletenode(head,tail,6);
+    cout<<endl;
+    print(head);
+    Node* prev=NULL;
+    Node* curr=head;
+    head=reverse(prev,curr);
     cout<<endl;
     print(head);
 }
