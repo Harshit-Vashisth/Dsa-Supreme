17fde0723f58ed0c0504a30b28aac7deccfceed2
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-05-22 11:51:17 +0530

    by harshit

--- 09 Linked List/02 linkedlist/doublylinked.cpp
+++ 09 Linked List/02 linkedlist/doublylinked.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-using namespace std;+
+using namespace std;
 class Node{
     public:
     int data;
@@ -33,5 +33,22 @@ int getLength(Node* head){
     }
 }
 int main(){
+        Node* head=new Node(10);
+        Node* a=new Node(20);
+        Node* b=new Node(30);
+        Node* c=new Node(40);
+        Node* d=new Node(50);
 
-}
+        head->next=a;
+        a->next=b;
+        b->next=c;
+        c->next=d;
+        d->prev=c;
+        c->prev=b;
+        b->prev=a;
+        a->prev=head;
+        
+
+        print(head);
+
+        }
