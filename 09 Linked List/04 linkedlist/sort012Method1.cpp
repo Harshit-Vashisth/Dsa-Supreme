659e0e6772013838fe774e76a668392f658423ba
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-05-26 16:41:30 +0530

    by harshot

--- 09 Linked List/04 linkedlist/sort012Method1.cpp
+++ 09 Linked List/04 linkedlist/sort012Method1.cpp
@@ -15,41 +15,90 @@ class Node{
         this->next=NULL;
     }
 };
-int length(Node* head){
+void print(Node* head){
     Node* temp=head;
-    int i=0;
     while(temp!=NULL){
+        cout<<temp->data;
         temp=temp->next;
-        i++;
     }
-    return i;
 }
-bool circular(Node* head){
+Node* sort(Node* head){
     if(head==NULL)
-    return false;
+    return NULL;
     if(head->next==head)
-    return true;
+    return head;
 
-    Node* temp=head;
-    while(temp->next!=head||temp!=NULL){
-        temp=temp->next;
+   Node* zerohead=new Node(-1);
+   Node* zerotail=zerohead;
+
+   Node* onehead=new Node(-1);
+   Node* onetail=onehead;
+
+   Node* twohead=new Node(-1);
+   Node* twotail=twohead;
+
+   Node* curr=head;
+   while(curr!=NULL){
+    if(curr->data==0)
+    {
+        Node* temp=curr;
+        curr=curr->next;
+        temp->next=NULL;
+        zerotail->next=temp;
+        zerotail=temp;
     }
-   if(temp->next==head)
-   return true;
-   return false;
+    else if(curr->data==1)
+    {
+        Node* temp=curr;
+        curr=curr->next;
+        temp->next=NULL;
+        onetail->next=temp;
+        onetail=temp;
+    }
+    else if(curr->data==2)
+    {
+        Node* temp=curr;
+        curr=curr->next;
+        temp->next=NULL;
+        twotail->next=temp;
+        twotail=temp;
+    }
+   }
+    Node* temp=onehead;
+    onehead=onehead->next;
+    temp->next=NULL;
+    delete temp;
+
+     temp=twohead;
+    twohead=twohead->next;
+    temp->next=NULL;
+    delete temp;
+
+   if(onehead!=NULL){
+    zerotail->next=onehead;
+    onetail->next=twohead;
+   }
+   else{
+    onetail->next=twohead;
+   } n 
+   temp=zerohead;
+    zerohead=zerohead->next;
+    temp->next=NULL;
+    delete temp;
+    return zerohead;
 }
 int main(){
-    Node* head=new Node(10);
-    Node* a=new Node(20);
-    Node* b=new Node(30);
-    Node* c=new Node(40);
-    Node* d=new Node(50);
-    Node* e=new Node(60);
+    Node* head=new Node(0);
+    Node* a=new Node(1);
+    Node* b=new Node(2);
+    Node* c=new Node(0);
+    Node* d=new Node(1);
+    Node* e=new Node(1);
     head->next=a;
     a->next=b;
     b->next=c;
     c->next=d;
     d->next=e;
-    e->next=head;
-    cout<<circular(head);
+    head=sort(head);
+    print(head);
 }
