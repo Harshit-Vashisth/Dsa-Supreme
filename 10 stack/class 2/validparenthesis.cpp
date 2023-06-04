a0ed33383c6e868b48d1bbe86c3ccece6b3cc604
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-04 15:07:38 +0530

    Create validparenthesis.cpp

---
+++ 10 stack/class 2/validparenthesis.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool valid(string s){
+    cout<<s;
+    }
+int main(){
+    string s="{}{}{";
+    cout<<s<<endl;
+    
+    if(valid(s))
+    cout<<"It is valid";
+    else 
+    cout<<"Its not valid";
+}
