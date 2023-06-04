8552ee0a20125102dfe617623934700f66972032
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-04 14:54:27 +0530

    by harshit

---
+++ 10 stack/class 2/insertatbottom.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stack<int>st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    st.push(60);
+    
+}
