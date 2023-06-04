9d202026b440a6d322276a69e9f165a6f69bb82d
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-04 14:59:14 +0530

    by harshit

--- 10 stack/class 2/insertatbottom.cpp
+++ 10 stack/class 2/insertatbottom.cpp
@@ -1,6 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+    void solve(stack<int> &st,int tar){
+        if(st.empty()){
+        st.push(tar);
+        return;}
+        int topele=st.top();
+        st.pop();
+        solve(st,tar);
+        st.push(topele);
+    }
+    void insertatBotton(stack<int> &st){
+        if(st.empty())
+        return;
+        int target=st.top();
+        st.pop();
+        solve(st,target);
+    }
+    void print(stack<int> st){
+        while(!st.empty()){
+            cout<<st.top()<<" ";
+            st.pop();
+        }
+    }
 int main(){
     stack<int>st;
     st.push(10);
@@ -9,5 +30,8 @@ int main(){
     st.push(40);
     st.push(50);
     st.push(60);
-    
+    print(st);
+    cout<<endl;
+    insertatBotton(st);
+    print(st);
 }
