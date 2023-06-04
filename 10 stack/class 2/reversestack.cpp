ed000d431d0ca9d413c63d4ac3d009aa2b39fc8a
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-04 15:04:05 +0530

    by harshit

---
+++ 10 stack/class 2/reversestack.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(stack<int> &st,int tar){
+        if(st.empty()){
+        st.push(tar);
+        return;}
+        int topele=st.top();
+        st.pop();
+        solve(st,tar);
+        st.push(topele);
+    }
+    void reverse(stack<int> &st){
+        if(st.empty())
+        return;
+        int target=st.top();
+        st.pop();
+        reverse(st);
+        solve(st,target);
+    }
+    void print(stack<int> st){
+        while(!st.empty()){
+            cout<<st.top()<<" ";
+            st.pop();
+        }
+    }
+int main(){
+    stack<int>st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    st.push(60);
+    print(st);
+    cout<<endl;
+    reverse(st);
+    print(st);
+}
