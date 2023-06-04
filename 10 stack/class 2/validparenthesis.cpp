a8ee6b32cb1236197581c56ca04ffbf558197ef2
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-04 16:19:06 +0530

    By harshit

--- 10 stack/class 2/validparenthesis.cpp
+++ 10 stack/class 2/validparenthesis.cpp
@@ -1,10 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool valid(string s){
-    cout<<s;
+
+    bool valid(string s) {
+        stack<char> st;
+        for(int i =0;i<s.length();i++){
+            char ch=s[i];
+            if(s[i]=='['||s[i]=='{'||s[i]=='(')
+                st.push(s[i]);
+            else
+            {
+                if(!st.empty()){
+                    if((st.top()=='('&&ch==')')||(st.top()=='['&&ch==']')||(st.top()=='{'&&ch=='}'))
+                    st.pop();
+                    else
+                return false;
+                }
+                else
+                return false;
+            }
+        }
+        if(st.empty())
+        return true;
+        else
+        return false;
     }
 int main(){
-    string s="{}{}{";
+    string s="{}{}({})";
     cout<<s<<endl;
     
     if(valid(s))
