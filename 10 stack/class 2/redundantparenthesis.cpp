af9b9c5654ebf77b8ef1594478437bcafba6ad15
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-05 12:35:58 +0530

    by harshit

--- 10 stack/class 2/redundantparenthesis.cpp
+++ 10 stack/class 2/redundantparenthesis.cpp
@@ -8,22 +8,33 @@ using namespace std;
     }
     bool valid(string s) {
         stack<char> st;
+        bool op=false;
         for(int i =0;i<s.length();i++){
             char ch=s[i];
             if(s[i]=='['||s[i]=='{'||s[i]=='('||s[i]=='+'||s[i]=='-'||s[i]=='*'||s[i]=='/'||s[i]=='%')
                 st.push(s[i]);
             else
             {
-                if(ch<'a'&&ch>'z'){
-                    bool op=false;
+                
+                if (s[i] < 'A' || s[i] > 'Z' && s[i] < 'a'|| s[i] > 'z'){
+                    
+                   
                     if((ch==')'||ch==']'||ch=='}')&&!st.empty()){
-                        while(st.top()!='('||st.top()!='{'||st.top()!='[')
+                       
+                        while(st.top()=='+'||st.top()=='*'||st.top()=='-'||st.top()=='/'||st.top()=='%')
                         {
+                            print(st);
+                              cout<<endl<<ch<< "  Dg  "<<st.top();
                             op=true;
                             st.pop();
                         }
-                        if((st.top()=='('&&ch==')')||(st.top()=='['&&ch==']')||(st.top()=='{'&&ch=='}'))
-                    st.pop();
+                        if(((st.top()=='('&&ch==')')||(st.top()=='['&&ch==']')||(st.top()=='{'&&ch=='}'))&&op==true){
+                            
+                             st.pop();
+                             op =false;
+                             cout<<"\nSF\n";
+                             print(st);
+                             cout<<"\n";}
                     else
                     return false;
                     }
@@ -33,6 +44,7 @@ using namespace std;
             }
         }
         
+        
         if(st.empty())
         return true;
         else
@@ -40,7 +52,7 @@ using namespace std;
     }
        
 int main(){
-    string s="(a+b)";
+    string s="(a/(a*a+b))";
     cout<<s<<endl;
      
     if(valid(s))
