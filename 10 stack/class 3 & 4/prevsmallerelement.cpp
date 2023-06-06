5967325bfa50b054f230f191fc97ab772d056510
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-06 14:04:51 +0530

    by harshit

--- 10 stack/class 3 & 4/prevsmallerelement.cpp
+++ 10 stack/class 3 & 4/prevsmallerelement.cpp
@@ -4,19 +4,20 @@ using namespace std;
 vector<int> prevsmaller(vector<int> &v){
     stack<int> st;
     st.push(-1);
-    vector<int> ans(st.size());
+    vector<int> ans(v.size());
     for(int i=0;i<v.size();i++){
-        while(st.top()>v[i]){
+        int curr=v[i];
+        while(st.top()>=curr){
             st.pop();
         }
         ans[i]=st.top();
-        st.push(v[i]);
+        st.push(curr);
     }
     return ans;
 }
 void print(vector<int> ans){
     for(int i=0;i<ans.size();i++){
-        cout<<ans[i];
+        cout<<ans[i]<<' ';
     }
 }
 int main(){
@@ -26,6 +27,8 @@ int main(){
     v.push_back(4);
     v.push_back(6);
     v.push_back(1);
+    print(v);
+    cout<<endl;
     vector<int> ans=prevsmaller(v);
     print(ans);
 }
