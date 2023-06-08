47ad9e55999f62f067a2fc4fcce491b20f37108e
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-08 21:53:48 +0530

    Update swaptomakestringbalance.cpp

---
+++ 10 stack/assignment/swaptomakestringbalance.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minSwaps(string s) {
+        stack<char> st;
+        for(char &ch:s){
+          if(ch=='[')  st.push(ch);
+          else if(!st.empty()) st.pop();
+        }
+        return (st.size()+1)/2;
+    }
+};
