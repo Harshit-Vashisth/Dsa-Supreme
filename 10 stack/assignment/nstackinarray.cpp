48632c1fc8519d6c142f7653cf4edc6a7bf979fb
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-10 00:33:41 +0530

    Update nstackinarray.cpp

--- 10 stack/assignment/nstackinarray.cpp
+++ 10 stack/assignment/nstackinarray.cpp
@@ -1,9 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Stack{
+class NStack{
+    public:
     int *a,*next ,*top;
-    int index;
+    int size;
     int freespot;
     int n;
-    
+    NStack(int n,int s){
+        this->n=n;
+        this->size=s;
+        freespot=0;
+        a=new int [size];
+        next=new int [size];
+        top=new int[n];
+        for(int i=0;i<n;i++)
+            top[i]=-1;
+        for(int i=0;i<size;i++)
+            next[i]=i+1;
+            next[size-1]=-1;
+    }
+    bool push(int X,int m){
+        if(freespot==-1)
+        return false;
+        int index=freespot;
+        freespot=next[index];
+        a[index]=X;
+        next[index]=top[m-1];
+        top[m-1]=index;
+        return true;
+    }
+    int pop(int m){
+        if(top[m]==-1)
+            return -1;
+
+        int index=top[m-1];
+        top[m-1]=next[index];
+        int poped=a[index];
+        next[index]=freespot;
+        return poped;
+    }
+    ~NStack(){
+        delete []a;
+        delete []top;
+        delete []next;
+    }
+};
+int main(){
+    NStack s(3,6);
+    s.push(10,1);
+    s.push(20,1);
+    s.push(30,1);
+    s.pop(1);
+    s.pop(1);
 }
