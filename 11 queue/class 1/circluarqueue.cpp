be4111378032ebb76cf4adadfb16c1888642f9d0
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-16 23:27:19 +0530

    by harshit

--- 11 queue/class 1/circluarqueue.cpp
+++ 11 queue/class 1/circluarqueue.cpp
@@ -4,16 +4,17 @@ class Circularqueue{
     public :
     int *arr,size,front,rear;
     Circularqueue(int size){
+        arr=new int[size];
         this->size=size;
         front=-1;
         rear=-1;
     }
     void push(int data){
-        if((front==0&&rear==size-1)||(rear==front-1)){
+        if((front==-0&&rear==size-1)||(rear==front-1)){
             cout<<"Overflow queue"<<endl;
             return ;
         }
-        else if(front==-1)
+        else if(front==-1&&rear==-1)
             front=rear=0;
         else if ((rear==size-1)&&(front!=0))
             rear=0;
@@ -38,4 +39,30 @@ class Circularqueue{
             front++;
         }
     }
+    int top(){
+        if(front==-1)
+            return -1;
+        return arr[front];
+    }
 };
+int main(){
+    Circularqueue q(10);
+    q.push(10);
+     q.push(20);
+     q.push(30);
+     q.push(40);
+     q.push(50);
+     q.push(60);
+     
+    cout<<"popping the element"<<endl;
+    q.pop();
+    cout<<q.top()<<endl;
+    q.pop();
+    cout<<q.top()<<endl;
+    q.pop();
+    cout<<q.top()<<endl;
+    q.pop();
+    q.pop();
+    q.pop();
+    q.pop();
+}
