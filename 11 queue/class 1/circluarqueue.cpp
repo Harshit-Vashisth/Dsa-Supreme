8326f911e8e0fee70974e071f4286830fdba65bc
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-16 23:18:32 +0530

    Update circluarqueue.cpp

--- 11 queue/class 1/circluarqueue.cpp
+++ 11 queue/class 1/circluarqueue.cpp
@@ -3,5 +3,39 @@ using namespace std;
 class Circularqueue{
     public :
     int *arr,size,front,rear;
-    void 
-}
+    Circularqueue(int size){
+        this->size=size;
+        front=-1;
+        rear=-1;
+    }
+    void push(int data){
+        if((front==0&&rear==size-1)||(rear==front-1)){
+            cout<<"Overflow queue"<<endl;
+            return ;
+        }
+        else if(front==-1)
+            front=rear=0;
+        else if ((rear==size-1)&&(front!=0))
+            rear=0;
+        else 
+            rear++;
+        arr[rear]=data;
+    }
+    void pop(){
+        if(front==-1)
+        cout<<"Underflow"<<endl;
+        else if(front==rear){
+            arr[front]=-1;
+            front=rear=-1;
+            }
+        else if(front==size-1){
+            arr[front]=-1;
+            front=0;
+        }
+        else
+        {
+            arr[front]=-1;
+            front++;
+        }
+    }
+};
