501f816f123110ae88c01f623be4e2c22430e39c
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-17 00:18:48 +0530

    by harshit

--- 11 queue/class 1/deque.cpp
+++ 11 queue/class 1/deque.cpp
@@ -16,15 +16,16 @@ class Deque{
         }
         else if(front==-1)
             front=rear=0;
-        else if(rear==size-1&&front!=0)
+        else if(rear==size-1 && front!=0)
             rear=0;
         else    
             rear++;
         arr[rear]=data;
+                    
     }
     void pushfront(int data){
-        if((front==0)&&(rear=size-1)){
-            cout<<"Underflow"<<endl;
+        if(front == 0 && rear == size-1){
+            cout<<"Overflow"<<endl;
             return;
         }
         else if(front==-1)
@@ -71,4 +72,75 @@ class Deque{
             rear--;
         }
     }
+    void print() {
+                for(int i=0; i<size; i++) {
+                        cout << arr[i] << " ";
+                }
+                cout << endl;
+        }
 };
+int main(){
+    Deque dq(10);
+  dq.print();
+
+  dq.pushrear(10);
+  dq.pushrear(20);
+  dq.pushrear(30);
+  dq.pushrear(40);
+  dq.pushrear(50);
+  dq.pushrear(60);
+  dq.pushrear(70);
+  dq.pushrear(80);
+
+  dq.print();
+
+  dq.poprear();
+  dq.print();
+
+  dq.poprear();
+  dq.print();
+
+  dq.popfront();
+  dq.print();
+
+    dq.popfront();
+  dq.print();
+
+    dq.popfront();
+  dq.print();
+
+  dq.pushfront(101);
+  dq.print();
+
+  dq.pushfront(102);
+  dq.print();
+
+  dq.pushfront(103);
+  dq.print();
+
+    dq.pushfront(104);
+  dq.print();
+
+  dq.popfront();
+  dq.print();
+
+  dq.popfront();
+  dq.print();
+
+  dq.pushrear(201);
+  dq.print();
+
+    dq.pushrear(202);
+  dq.print();
+
+    dq.pushrear(203);
+  dq.print();
+    dq.pushrear(204);
+  dq.print();
+
+    dq.pushrear(205);
+  dq.print();
+
+
+  return 0;
+}
