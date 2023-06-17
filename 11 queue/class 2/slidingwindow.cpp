f5217f96064979cbe856870db9b048bae599fdc1
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-18 01:20:20 +0530

    Update slidingwindow.cpp

--- 11 queue/class 2/slidingwindow.cpp
+++ 11 queue/class 2/slidingwindow.cpp
@@ -1,2 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int main(){
+    int main(){
+    queue<int> q;
+    q.push(10);
+    q.push(-20);
+    q.push(-30);
+    q.push(40);
+    q.push(-50);
+    q.push(-20);
+    q.push(70);
+    q.push(80);
+    q.push(90);
+    q.push(-2);
+    q.push(100);
+    
+}
