66941e45f0f38e6eba9b7ccee7a3a570a8503c91
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-17 17:25:02 +0530

    by harshit

--- 11 queue/class 2/reversequeue.cpp
+++ 11 queue/class 2/reversequeue.cpp
@@ -3,7 +3,10 @@ using namespace std;
 void reverse(queue<int> &q){
     if(q.empty())
         return ;
-    
+    int ele=q.front();
+    q.pop();
+    reverse(q);
+    q.push(ele);
 }
 int main(){
     queue<int> q;
@@ -14,6 +17,7 @@ int main(){
     q.push(50);
     q.push(60);
     q.push(70);
+    cout<<q.front()<<endl;
     reverse(q);
-
+    cout<<q.front()<<endl;
 }
