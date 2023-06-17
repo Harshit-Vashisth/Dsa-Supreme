3860a120980ebfeb45eae27730aa3c2a3be6954d
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-17 17:22:57 +0530

    Create reversequeue.cpp

---
+++ 11 queue/class 2/reversequeue.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void reverse(queue<int> &q){
+    if(q.empty())
+        return ;
+    
+}
+int main(){
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    q.push(60);
+    q.push(70);
+    reverse(q);
+
+}
