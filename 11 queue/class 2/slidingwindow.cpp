b6a61e20176c625ef7142950f83da3a706e8b36a
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-18 01:33:10 +0530

    by harshit

--- 11 queue/class 2/slidingwindow.cpp
+++ 11 queue/class 2/slidingwindow.cpp
@@ -1,19 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int main(){
-    queue<int> q;
-    q.push(10);
-    q.push(-20);
-    q.push(-30);
-    q.push(40);
-    q.push(-50);
-    q.push(-20);
-    q.push(70);
-    q.push(80);
-    q.push(90);
-    q.push(-2);
-    q.push(100);
-    
+void solve(int arr[],int n, int k ) {
+       queue<int> q;
+       for(int i=0;i<k;i++){
+        if(arr[i]<0)
+        q.push(i);
+       }
+       for(int i=k;i<n;i++){
+        if(q.empty())
+                cout<<'0'<<" ";
+        else    
+            cout<<arr[q.front()]<<" ";
+
+        if(!q.empty()&&(i-q.front()>=k))
+                q.pop();
+        if(arr[i]<0)
+                q.push(i);
+       }
+       if(q.empty()) cout<<'0'<<" ";
+       else
+                cout<<arr[q.front()]<<" ";
+}
+int main() {
+  int arr[] = {12,-1,-7,8,-15,30,16,28};
+  int size = 8;
+  
+  int k = 3;
+
+  solve(arr,size,k);
+  return 0;
 }
