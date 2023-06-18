2314daf8131d55e52f963d2d496835d137e8bedd
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-19 03:02:55 +0530

    by harshit

--- 11 queue/class 3/firstnonrepeatchar.cpp
+++ 11 queue/class 3/firstnonrepeatchar.cpp
@@ -5,15 +5,17 @@ void solve(string s ) {
     int arr[26]={0};
     string ans;
     queue<char> q;
-    for(int i =0;i<s.size();i++){
+    for(int i =0;i<s.length();i++){
         char ch=s[i];
         q.push(ch);
         arr[ch-'a']++;
         while(!q.empty()){
             if(arr[q.front()-'a']>1)
                 q.pop();
-            else    
-                ans.push_back(arr[q.front()]);
+            else    {
+                ans.push_back(q.front());
+                break;
+                }
         }
         if(q.empty())
             ans.push_back('#');
@@ -21,7 +23,7 @@ void solve(string s ) {
     cout<<ans;
 }
 int main() {
-  string s="aabcdef";
+  string s="aabbcc";
   
   solve(s);
   return 0;
