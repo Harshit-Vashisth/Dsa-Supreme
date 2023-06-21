cfc3d989c2cb5f2136a4d9f5caf88d87faca44d9
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-22 04:14:15 +0530

    Update printdataLEVELWISE.cpp

---
+++ 12 tree/class 1/printdataLEVELWISE.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+    this->data=data;
+    left=NULL;
+    right=NULL;
+}
+};
+Node* buildtree(){
+   int data;
+   cin>>data;
+   if(data==-1)
+   return NULL;
+   
+   Node* root=new Node(data);
+   cout<<"Enter the left value"<<endl;
+   root->left=buildtree();
+   cout<<"Enter the right value"<<endl;
+   root->right=buildtree();
+}
+void levelordertraversal(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* temp=q.front();
+        q.pop();
+        cout<<temp->data<<" ";
+        if(temp->left)
+            q.push(temp->left);
+        if(temp->right)
+            q.push(temp->right);
+        
+    }
+}
+int main(){
+    Node* root=NULL;
+    root=buildtree();
+    levelordertraversal(root);
+}
