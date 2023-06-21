9f6217781fe3167865c9ca11b9c50902e84156f6
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-22 04:47:52 +0530

    by harshit

---
+++ 12 tree/class 1/preorder-NLR.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    Node* left;
+    Node* right;
+    int data;
+    Node(int data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+Node* buildtree(){
+    int data;
+    cin>>data;
+    if(data==-1)
+        return NULL;
+    Node* root=new Node(data);
+    cout<<"LEFT\n";
+    root->left=buildtree();
+    cout<<"Right\n";
+    root->right=buildtree();
+}
+void inorder(Node* root){
+   if(root==NULL)
+    return;
+    cout<<root->data<<" ";
+    inorder(root->left);
+    inorder(root->right);
+}
+int main(){
+    Node* root=NULL;
+    root=buildtree();
+    inorder(root);
+}
