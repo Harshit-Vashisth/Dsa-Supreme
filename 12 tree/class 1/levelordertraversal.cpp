6947f3f75c242f4849835206ae6d11cf886fea9d
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-22 04:06:42 +0530

    by harshit

--- 12 tree/class 1/levelordertraversal.cpp
+++ 12 tree/class 1/levelordertraversal.cpp
@@ -1,2 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+    this->data=data;
+    left=NULL;
+    right=NULL;
+}
+};
+Node* buildtree(){
+   int data;
+   cin>>data;
+   if(data==-1)
+   return NULL;
+   
+   Node* root=new Node(data);
+   cout<<"Enter the left value"<<endl;
+   root->left=buildtree();
+   cout<<"Enter the right value"<<endl;
+   root->right=buildtree();
+}
+void levelordertraversal(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* temp=q.front();
+        q.pop();
+        cout<<temp->data<<" ";
+        if(root->left)
+            q.push(root->left);
+        if(root->right)
+            q.push(root->right);
+        
+    }
+}
+int main(){
+    Node* root=NULL;
+    root=buildtree();
+    levelordertraversal(root);
+}
