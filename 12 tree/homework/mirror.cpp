1da94f8db8d44e078e346a25e7b97c067d9fbcfc
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-23 03:45:49 +0530

    Update mirror.cpp

--- 12 tree/homework/mirror.cpp
+++ 12 tree/homework/mirror.cpp
@@ -1,16 +1,39 @@
-#inc
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    Node* left;
+    Node* right;
+    int data;
+    Node(int data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+Node* buildtree(){
+    int data;
+    cin>>data;
+    if(data==-1)
+        return NULL;
+    Node* root=new Node(data);
+    cout<<"LEFT\n";
+    root->left=buildtree();
+    cout<<"Right\n";
+    root->right=buildtree();
+}
 class Solution {
     public:
-     bool isSameTree(TreeNode* p, TreeNode* q) {
+     bool isSameTree(Node* p, Node* q) {
         if(q==NULL&&p==NULL)
             return true;
         if(q==NULL || p==NULL)
             return false;
-           if(q->val!=p->val)
+           if(q->data!=p->data)
                 return false;
         return (isSameTree(p->left,q->right)&&isSameTree(p->right,q->left));
     }
-    bool isSymmetric(TreeNode* root) {
+    bool isSymmetric(Node* root) {
         if(root==NULL)
             return true;
 
