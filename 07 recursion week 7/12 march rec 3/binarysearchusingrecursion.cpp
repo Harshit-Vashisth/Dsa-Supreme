fd12fd6ff9b016482784e9326821485d0b8e6775
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-22 14:56:22 +0530

    by harshit

--- 07 recursion week 7/12 march rec 3/binarysearchusingrecursion.cpp
+++ 07 recursion week 7/12 march rec 3/binarysearchusingrecursion.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 int binarysearch(vector<int>& arr,int s,int e,int tar){
     if(s>e)
-    return -1;
+        return -1;
     int mid=s+(e-s)/2;
     if(arr[mid]==tar)
     return mid;
-    if(arr[mid]<tar)
-    return binarysearch(arr,mid+1,e,tar);
+    if(arr[mid]>tar)
+        return binarysearch(arr,s,mid-1,tar);
     else
-    return binarysearch(arr,s,mid-1,tar);
+       return  binarysearch(arr,mid+1,e,tar);
 }
 int main(){
-    vector<int> v{10,20,30,60,50,99,4};
+    vector<int> v{10,20,30,60,70,99};
     int n=7,tar;
-    int s=0,e=*max_element(v.begin(),v.end());
+    int s=0,e=n-1;
     cout<<"Enter the value of key"<<endl;
     cin>>tar;
     cout<<"Element present at :> "<<binarysearch(v,s,e,tar);
