9f15cdd871b7705ff6e987c5e194b4bda10e5d5f
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-23 02:26:48 +0530

    Update checkIdentical.cpp

--- 12 tree/homework/checkIdentical.cpp
+++ 12 tree/homework/checkIdentical.cpp
@@ -1,1 +1,33 @@
 #include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    Node* left;
+    Node* right;
+    int data;
+    Node(int data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+Node* buildtree(){
+    int data;
+    cin>>data;
+    if(data==-1)
+        return NULL;
+    Node* root=new Node(data);
+    cout<<"LEFT\n";
+    root->left=buildtree();
+    cout<<"Right\n";
+    root->right=buildtree();
+}
+bool isSameTree(Node* p, Node* q) {
+        if(q==NULL&&p==NULL)
+            return true;
+        if(q==NULL&&p!=NULL)
+            return false;
+        if(q!=NULL &&p==NULL)
+            return false;
+        return ((q->data==p->val)&&isSameTree(p->left,q->left)&&isSameTree(p->right,q->right));
+    }
