a69b6d5a0cc21bc2708fdaa8dcffa135133a4a7b
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-25 02:47:48 +0530

    Update printdataLEVELWISE.cpp

--- 12 tree/class 1/printdataLEVELWISE.cpp
+++ 12 tree/class 1/printdataLEVELWISE.cpp
@@ -25,25 +25,6 @@ Node* buildtree(){
 }
 void printlevelwise(Node* root){
      queue<Node*> q;
-     q.push(root);
-     q.push(NULL);
-     while(!q.empty()){
-        Node* temp=q.front();
-        q.pop();
-        if(temp==NULL){
-            cout<<endl;
-            if(!q.empty())
-                q.push(NULL);
-        }
-        else{
-            cout<<temp->data<<"  ";
-            if(temp->left)
-                q.push(temp->left);
-            if(temp->right)
-                q.push(temp->right);
-        }
-
-     }
 
 }
 int main(){
