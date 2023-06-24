ceec2c470c9334c305d945ebb52ce347c729d37c
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-24 22:46:53 +0530

    harshit

--- 07 recursion week 7/15 march rec 4/targetmaking_coinchangeproblem.cpp
+++ 07 recursion week 7/15 march rec 4/targetmaking_coinchangeproblem.cpp
@@ -5,8 +5,7 @@ int solve(vector<int>& arr,int tar){
     return 0;
     if(tar<0)
     return INT_MAX;
-    int ans=INT_MAX;
-    int mini=ans;
+    int mini=INT_MAX;
     for(int i=0;i<arr.size();i++){
         int ans=solve(arr,tar-arr[i]);
         if(ans!=INT_MAX)
@@ -16,7 +15,8 @@ int solve(vector<int>& arr,int tar){
 }
 int main(){
     //you have to tell the min number of eleements required to reach target sum 
-    vector<int> v={1,2,3,4};
-    int tar=7;
+    vector<int> v={1,2};
+    int tar=9;
     cout<<solve(v,tar);
 }
+  
