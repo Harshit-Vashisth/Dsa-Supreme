31eb23a9874ee883aeab65be6ca2660255306168
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-26 04:36:01 +0530

    Update convertToBinarysumtree.cpp

--- 12 tree/class 2/convertToBinarysumtree.cpp
+++ 12 tree/class 2/convertToBinarysumtree.cpp
@@ -1,2 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+class Node{
+    public:
+    Node* left;
+    Node* right;
+    int data;
+    Node(int data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+Node* buildtree(){
+    int data;
+    cin>>data;
+    if(data==-1)
+        return NULL;
+    Node* root=new Node(data);
+    cout<<"LEFT\n";
+    root->left=buildtree();
+    cout<<"Right\n";
+    root->right=buildtree();
+}
