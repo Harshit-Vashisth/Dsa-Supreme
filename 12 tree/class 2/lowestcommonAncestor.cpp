bf6b9dd3c6de75f543337fdf88ad29a20543a4ad
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-26 04:59:07 +0530

    Update lowestcommonAncestor.cpp

--- 12 tree/class 2/lowestcommonAncestor.cpp
+++ 12 tree/class 2/lowestcommonAncestor.cpp
@@ -1,2 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+class TreeNode{
+public:
+    TreeNode* left;
+    TreeNode* right;
+};
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* &root, TreeNode* p, TreeNode* q) {
+        if(root==NULL)
+            return NULL;
+        
+        if(root->val==p->val)
+            return p;
+        if(root->val==q->val)
+            return q;
+        TreeNode* leftans=lowestCommonAncestor(root->left,p,q);
+        TreeNode* rightans=lowestCommonAncestor(root->right,p,q);
+        
+        if(leftans==NULL&&rightans==NULL)
+            return NULL;
+        if(leftans!=NULL&&rightans==NULL)
+            return leftans;
+        if(leftans==NULL&&rightans!=NULL)
+            return rightans;
+        else
+            return root;
+    }
+};
