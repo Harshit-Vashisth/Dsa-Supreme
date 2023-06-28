b79683494fd605c1c3965ac3b3097681bc5447b0
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-06-29 04:00:02 +0530

    Update prepostorderbuild.cpp

--- 12 tree/class 3/prepostorderbuild.cpp
+++ 12 tree/class 3/prepostorderbuild.cpp
@@ -6,3 +6,30 @@ public:
     TreeNode* right;
     int val;
 };
+class Solution {
+public:
+  
+  TreeNode* construct(vector<int>& pre, vector<int>& post, int s1,int e1,int s2,int e2)
+  {
+    if(s1>e1 || s2>e2) return NULL;
+    TreeNode* root=new TreeNode(pre[s1]);
+    if(s1==e1) return root;
+    
+    int idx=s2;
+    while(pre[s1+1]!=post[idx])
+      idx++;
+    
+    int b=idx-s2+1;
+
+    root->left=construct(pre,post,s1+1,s1+b,s2,idx);
+    root->right=construct(pre,post,s1+b+1,e1,idx+1,e2-1);
+    
+    return root;
+    
+  }
+  
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        TreeNode* root= construct(preorder,postorder,0,preorder.size()-1,0,postorder.size()-1);
+      return root;
+    }
+};
