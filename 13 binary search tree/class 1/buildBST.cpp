04ae472bcac367521f767835d2f51ebd7c579e5d
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-02 01:04:17 +0530

    by harshit

--- 13 binary search tree/class 1/buildBST.cpp
+++ 13 binary search tree/class 1/buildBST.cpp
@@ -30,3 +30,21 @@ void takeInput(Node* root){
         cin>>data;
     }
 }
+void traversal(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* temp=q.front();
+        q.pop();
+        cout<<temp->data;
+        if(temp->left)
+            q.push(temp->left);
+        if(temp->right)
+            q.push(temp->right);
+    }
+}
+int main(){
+    Node* root=NULL;
+    takeInput(root);
+    traversal(root);
+}
