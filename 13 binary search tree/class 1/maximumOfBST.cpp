f7bb41394786d4b859a00f116c0157405c9b9fa1
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-02 01:47:06 +0530

    Create maximumOfBST.cpp

---
+++ 13 binary search tree/class 1/maximumOfBST.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+        this->data=data;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+Node* insertBST(Node* root,int data){
+    if(root==NULL){ 
+        root=new Node(data);
+        return root;
+    }
+    if(root->data>data)
+        root->left=insertBST(root->left,data);
+    else
+        root->right=insertBST(root->right,data);
+    return root;
+}
+void takeInput(Node* &root){
+    int data;
+    cout<<"Enter the elements"<<endl;
+    cin>>data;
+    while(data!=-1){
+       root= insertBST(root,data);
+       cout<<"Enter the data"<<endl;
+        cin>>data;
+    }
+}
+void traversal(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        Node* temp=q.front();
+        q.pop();
+        
+        if(temp==NULL)
+        {
+            cout<<endl;
+            if(!q.empty())
+                q.push(NULL);
+        }
+        else{
+            cout<<temp->data<<" ";
+        if(temp->left)
+            q.push(temp->left);
+        if(temp->right)
+            q.push(temp->right);
+        }
+    }
+}
+
+int maximum(Node* root){
+    if(root==NULL)
+    return -1;
+    while(root->right)
+        root=root->right;
+    return root->data;
+}
+int main(){
+    Node* root=NULL;
+    takeInput(root);
+    cout<<"Printing the root";
+    traversal(root);
+    cout<<endl<<"The minimum value is "<<maximum(root);
+}
