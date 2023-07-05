e83e4fba053212e597e77fda7feefa5d5f7066e3
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-05 16:41:26 +0530

    Update minCostTicket.cpp

--- 07 recursion week 7/Homework/minCostTicket.cpp
+++ 07 recursion week 7/Homework/minCostTicket.cpp
@@ -1,2 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+    int tickets(vector<int>& days,vector<int>& costs,int i){
+        if(i>=days.size())
+            return 0;
+        
+        int cost1=costs[0]+tickets(days,costs,i+1);
+
+        int passend=days[i]+7-1;
+        int j=i;
+        while(j<days.size()&&days[j]<=passend)
+            j++;
+        int cost7=costs[1]+tickets(days,costs,j);
+
+        passend=days[i]+30-1;
+        j=i;
+        while(j<days.size()&&days[j]<=passend)
+            j++;
+        int cost30=costs[2]+tickets(days,costs,j);
+
+        return min(cost1,min(cost7,cost30));
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        return tickets(days,costs,0);
+    }
+};
