9b07beb2e124becc61690f1845894862a2ec8f12
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-05 16:41:53 +0530

    Update PerfectSquares.cpp

--- 07 recursion week 7/Homework/PerfectSquares.cpp
+++ 07 recursion week 7/Homework/PerfectSquares.cpp
@@ -1,2 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+    int numsq(int n){
+          //base case
+        if(n==0) return 1;
+        if(n<0) return 0;
+        int ans=INT_MAX;
+        int i=1;
+        int end=sqrt(n);
+        while(i<=end){
+            int pref=i*i;
+            int num=1+numsq(n-pref);
+            if(ans>num)
+                ans=num;
+            i++;
+        }
+        return ans;
+    }
+    int numSquares(int n) {
+        return numsq(n)-1;
+    }
+};
