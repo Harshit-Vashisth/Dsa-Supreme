0c2ed8001f9719a3817b85873411d99e31f0cd08
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-05 16:42:45 +0530

    Update Wildcard.cpp

--- 07 recursion week 7/Homework/Wildcard.cpp
+++ 07 recursion week 7/Homework/Wildcard.cpp
@@ -1,2 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+    bool isMatch(string s,int si,string p,int pi){
+        //base case
+        if(si>=s.size()&&pi>=p.size())
+        return true;
+        if(si>=s.size()&&pi<p.size()){
+            while(pi<p.size()){
+                if(p[pi]!='*')
+                return false;
+                pi++; 
+            }
+             return true;
+        }
+
+
+        if((s[si]==p[pi])||(p[pi]=='?'))
+            return isMatch(s,si+1,p,pi+1);
+        if(p[pi]=='*'){
+            bool a= isMatch(s,si,p,pi+1);
+            bool b= isMatch(s,si+1,p,pi);
+            return a||b;
+        }
+        return false;
+    }
+
+    bool isMatch(string s, string p) {
+        return isMatch(s,0,p,0);
+    }
+};
