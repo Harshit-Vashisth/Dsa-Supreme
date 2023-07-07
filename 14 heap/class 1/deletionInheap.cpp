4748c7b41e5472eed2b80673bf116f1f1b658f0b
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-08 04:39:27 +0530

    Update deletionInheap.cpp

---
+++ 14 heap/class 1/deletionInheap.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Heap{
+    public:
+    int arr[100];
+    int size;
+    Heap(){
+        size=0;
+    }
+    void insert(int value){
+        size=size+1;
+        int index=size;
+        arr[index]=value;
+        while(index>1){
+            int parent=index/2;
+            if(arr[index]>arr[parent]){
+                swap(arr[index],arr[parent]);
+                index=parent;
+            }
+            else break;
+        }
+    }
+};
+int main(){
+    Heap h;
+    h.arr[0]=-1;
+    h.arr[1]=100;
+    h.arr[2]=50;
+    h.arr[3]=60;
+    h.arr[4]=40;
+    h.arr[5]=45;
+    h.size=5;
+    cout<<"printing the array"<<endl;
+    for(int i=0;i<=h.size;i++)
+        cout<<h.arr[i]<<" ";
+    h.insert(110);
+    cout<<endl;
+    for(int i=0;i<=h.size;i++)
+        cout<<h.arr[i]<<" ";
+
+}
