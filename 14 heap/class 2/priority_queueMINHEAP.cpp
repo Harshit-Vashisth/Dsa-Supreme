4beb22f42cb215ce7819c70d3006695871bce7ba
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-09 17:15:07 +0530

    by harshit

---
+++ 14 heap/class 2/priority_queueMINHEAP.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+priority_queue<int,vector<int>,greater<int>> pq;
+pq.push(3);
+pq.push(4);
+pq.push(6);
+pq.push(7);
+pq.push(2);
+while(!pq.empty()){
+    cout<<pq.top();
+    pq.pop();
+}
+}
