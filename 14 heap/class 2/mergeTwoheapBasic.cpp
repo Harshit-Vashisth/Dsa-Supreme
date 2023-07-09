3328cc38a3335d845189211c2dc730f7dc51c8fc
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-09 17:55:24 +0530

    Update mergeTwoheapBasic.cpp

--- 14 heap/class 2/mergeTwoheapBasic.cpp
+++ 14 heap/class 2/mergeTwoheapBasic.cpp
@@ -1,2 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
+void mergeheap(priority_queue<int> pq,int arr1[],int arr2[]){
+    
+}
+int main (){
+    int arr1[]={12,1,4,5,2,6,10};
+    int arr2[]={3,7,16,8,9};
+    priority_queue<int> pq;
+}
