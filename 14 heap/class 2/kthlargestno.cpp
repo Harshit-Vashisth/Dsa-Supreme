bb3090eb995e0afa4ad6915b02418a277d4c18d7
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-09 17:44:51 +0530

    by harshit

--- 14 heap/class 2/kthlargestno.cpp
+++ 14 heap/class 2/kthlargestno.cpp
@@ -1,20 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 int getkthlargest(int arr[],int n,int k){
-    priority_queue<int> pq;
+    priority_queue<int,vector<int>,greater<int>> pq;
     for(int i =0;i<k;i++)
         pq.push(arr[i]);
 
     for(int i=k;i<n;i++){
-        if(arr[i]>pq.top())
+        if(arr[i]>pq.top()){
             pq.pop();
+            pq.push(arr[i]);
+            }
     }
     return pq.top();
 }
 int main (){
     int arr[]={10,5,20,4,150};
     int n=5;
-    int k=2;
+    int k=3;
     int ans=getkthlargest(arr,n,k);
     cout<<ans;
 }
