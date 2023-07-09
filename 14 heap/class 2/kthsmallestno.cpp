530239ed343ec7fd90e7c104e8775692fb5f1921
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-09 17:25:07 +0530

    Update kthsmallestno.cpp

--- 14 heap/class 2/kthsmallestno.cpp
+++ 14 heap/class 2/kthsmallestno.cpp
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+int getkthsmallest(int arr[],int n,int k){
+    priority_queue<int> pq;
+    for(int i =0;i<k;i++)
+        pq.push(arr[i]);
+}
 int main (){
-    s
+    int arr[]={10,5,20,4,150};
+    int n=5;
+    int k=2;
+    int ans=getkthsmallest(arr,n,k);
 }
