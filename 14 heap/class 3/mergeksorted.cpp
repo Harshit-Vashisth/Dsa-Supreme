b1e9fd8861041c0a4f0f05dfbc35742806b1ac60
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-11 01:04:37 +0530

    Create mergeksorted.cpp

---
+++ 14 heap/class 3/mergeksorted.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data,row,col;
+    Node(int val,int r,int c){
+        data=val;
+        row=r;
+        col=c;
+    }
+};
+class compare{
+    public:
+    bool operator()(Node* a,Node* b){
+        return a->data>b->data;}
+};
+vector<int> MergeSort(int arr[][4],int k,int n){
+    priority_queue<Node*,vector<Node*>,compare> minheap;
+    for(int i=0;i<k;i++){
+        Node* temp=new Node(arr[i][0],i,0);
+        minheap.push(temp);
+    }
+    vector<int> ans;
+    while(!minheap.empty()){
+        Node* temp=minheap.top();
+        int top=temp->data;
+        int toprow=temp->row;
+        int topcol=temp->col;
+        minheap.pop();
+        ans.push_back(top);
+        if(topcol+1<n){
+            Node* newnode=new Node(arr[toprow][topcol+1],toprow,topcol+1);
+            minheap.push(newnode);
+        }
+    }
+    return ans;
+}
