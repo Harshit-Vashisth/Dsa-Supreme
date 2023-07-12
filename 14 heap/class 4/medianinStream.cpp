11c87c92697c6923ac8f4c3f208d3a9687f43994
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-13 00:42:29 +0530

    Update medianinStream.cpp

--- 14 heap/class 4/medianinStream.cpp
+++ 14 heap/class 4/medianinStream.cpp
@@ -8,7 +8,7 @@ int sigum(int a,int b){
     if(a<b)
         return -1;
 }
-int median(double &median,priority_queue<int> &maxheap,priority_queue<int,vector<int>,greater<int>> &minheap, int element){
+void callmedian(double &median,priority_queue<int> &maxheap,priority_queue<int,vector<int>,greater<int>> &minheap, int element){
     switch(sigum(minheap.size(),maxheap.size())){
         case 0:
             if(element>median){
@@ -19,5 +19,28 @@ int median(double &median,priority_queue<int> &maxheap,priority_queue<int,vector
                 maxheap.push(element);
                 median=maxheap.top();
             }
+        case 1:
+            if(element>maxheap.top()){
+                minheap.push(maxheap.top());
+                maxheap.pop();
+                maxheap.push(element);
+                median=(maxheap.top()+minheap.top())/2.0;
+            }
+            else{
+                maxheap.push(element);
+                median=(maxheap.top()+minheap.top())/2.0;
+            }
+        case -1:
+    }}
+int main(){
+    int arr[10]={2,4,5,10,1,40,4,6,3,9};
+    int n=12;
+    double median=0;
+    priority_queue<int> maxheap;
+    priority_queue<int,vector<int> , greater<int>> minheap;
+    for(int i=0;i<n;i++){
+        int ele=arr[i];
+         callmedian(median,maxheap,minheap,ele);
+         cout<<median;
     }
 }
