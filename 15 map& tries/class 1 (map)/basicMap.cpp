8003d41a6b38d210bd5307c0a951ace65f2edebe
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-14 00:15:21 +0530

    Update basicMap.cpp

--- 15 map& tries/class 1 (map)/basicMap.cpp
+++ 15 map& tries/class 1 (map)/basicMap.cpp
@@ -1,2 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int main(){
+    unordered_map<int,string> mp;
+    mp[1]="head";
+    mp[3]="hello";
+    mp[2]="Once";
+    mp[5]="Not";
+    mp[4]="Working";
+    for(auto it:mp)
+        cout<<it.first<<"  "<<it.second;
+}
