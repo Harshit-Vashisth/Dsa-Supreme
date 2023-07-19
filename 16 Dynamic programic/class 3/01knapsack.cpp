2c4e6f866798a1a2eaf0d8296c00082dc3a19e12
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-19 16:10:55 +0530

    by harshit

--- 16 Dynamic programic/class 3/01knapsack.cpp
+++ 16 Dynamic programic/class 3/01knapsack.cpp
@@ -16,8 +16,28 @@ int usingrecursion(int weight[],int value[],int index,int capacity){
         int ans=max(include,exclude);
         return ans;
 }
+int Topdown(int weight[],int value[],int index,int capacity,vector<vector<int>> &dp){
+    if(index==0){
+        if(weight[0]<=capacity)
+            return value[0];
+        else
+            return 0;
+        }
+        if(dp[index][capacity]!=-1)
+            return dp[index][capacity];
+         int include=0;
+        if(weight[index]<=capacity)
+            include=value[index]+Topdown(weight,value,index-1,capacity-weight[index]),dp;
+        
+        int exclude=Topdown(weight,value,index-1,capacity,dp);
+        dp[index][capacity]=max(include,exclude);
+        return dp[index][capacity];
+   
+}
 int getMaxval(int weight[],int value[],int index,int capacity){
-    return usingrecursion(weight,value,index-1,capacity);
+    // return usingrecursion(weight,value,index-1,capacity);
+    vector<vector<int>> dp(index+1,(vector<int>(capacity+1,-1)));
+    return Topdown(weight,value,index,capacity,dp);
 }
 int main(){
     int weight[]={4,5,1};
