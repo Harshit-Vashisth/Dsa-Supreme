824b70cc21ab6dc7a1ca6e6a1f54c65adccb2a30
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-19 14:35:36 +0530

    Update 0,1knapsack.cpp

--- 16 Dynamic programic/class 3/0,1knapsack.cpp
+++ 16 Dynamic programic/class 3/0,1knapsack.cpp
@@ -1,3 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+// in dp whenu go right to left u feel it more easy 
+int usingrecursion(int weight[],int value[],int index,int capacity){
+    if(index==0){
+        if(weight[index]<=capacity)
+            return value[index];
+        else
+            return 0;
+        }
+        int include=value[index]+usingrecursion(weight,value,index-1,capacity-weight[index]);
+        
+        int exclude=usingrecursion(weight,value,index-1,capacity);
+        return max(include,exclude);
+}
+int getMaxval(int weight[],int value[],int index,int capacity){
+    return usingrecursion(weight,value,index,capacity);
+}
+int main(){
+    int weight[]={4,5,1};
+    int value[]={1,2,3};
+    int capacity=4;
+    int index=3;
+    cout<<getMaxval(weight,value,index,capacity);
+}
