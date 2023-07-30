7503155596cae6c8abc6e599bf4929a0352a1a14
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-31 03:42:04 +0530

    Update GraphBFS.cpp

--- 17 Graphs/class 1/GraphBFS.cpp
+++ 17 Graphs/class 1/GraphBFS.cpp
@@ -1,2 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+template<typename T>
+class Graph{
+    public:
+unordered_map<T,list<T>> adj;
+void addedge(T u,T v,bool direction){
+    //direction 0 undirected
+    //direction 1 directed
+    adj[u].push_back(v);
+    if(direction==0)
+        adj[v].push_back(u);
+}
+void printadj(){
+    for(auto i:adj){
+        cout<<i.first<<"->";
+        for(auto neigh:i.second){
+            cout<<neigh<<" ,";
+        }
+        cout<<endl;
+    }
+}
+void bfs()
+};
+int main(){
+    Graph<int> g;
+    g.addedge(0,1,1);
+    g.addedge(1,2,1);
+    g.addedge(2,1,1);
+    g.addedge(3,1,1);
+    g.addedge(3,4,1);
+    g.addedge(4,3,1);
+    g.printadj();
+}
