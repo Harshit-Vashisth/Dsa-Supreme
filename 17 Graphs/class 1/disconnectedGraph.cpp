f1e80202333934a0cee21d68e0b87e3aefd1a4af
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-31 04:14:38 +0530

    Create disconnectedGraph.cpp

---
+++ 17 Graphs/class 1/disconnectedGraph.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+template<typename T>
+class Graph{
+    public:
+   
+void addedge(T u,T v,bool direction){
+    //direction 0 undirected
+    //direction 1 directed
+    adj[u].push_back(v);
+    if(direction==0)
+        adj[v].push_back(u);
+}
+void printadj(){
+    for(auto i:adj){
+        cout<<i.first<<"->";
+        for(auto neigh:i.second){
+            cout<<neigh<<" ,";
+        }
+        cout<<endl;
+    }
+}
+void bfs(T src,unordered_map<T, bool> visited){
+    queue<T> q;
+    
+    q.push(src);
+    visted[src]=true;
+    while(!q.empty())
+    {
+        T front=q.front();
+        q.pop();
+        cout<<front<<" -> ";
+        for(auto i:adj[front]){
+            if(!visted[i]){
+                q.push(i);
+                visted[i]=true;
+            }
+        }
+    }
+}
+};
+int main(){
+    Graph<int> g;
+    g.addedge(0,1,0);
+    g.addedge(1,2,0);
+    g.addedge(2,1,0);
+    g.addedge(3,1,0);
+    g.addedge(3,4,0);
+    g.addedge(4,3,0);
+    g.printadj();
+    unordered_map<int, bool> visited;
+    for(int i=0;i<5;i++){
+        if(!visited[i])
+            g.bfs(i,visited);
+    }
+    
+}
