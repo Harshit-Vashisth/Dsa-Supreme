d47333bc2a87ccf339c523ddc23cfc84c04f71c7
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-31 03:08:27 +0530

    Create adjacencymatrixweight.cpp

---
+++ 17 Graphs/class 1/adjacencymatrixweight.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,e;
+    cout<<"Enter the number of node"<<endl;
+    cin>>n;
+    cout<<"Enter the number of edges"<<endl;
+    cin>>e;
+    int u,v,weight;
+    vector<vector<int>> adj(n,(vector<int>(n,0)));
+   for(int i=0;i<e;i++){
+        cin>>u>>v>>weight;
+        adj[u][v]=weight;
+   }
+   for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        cout<<adj[i][j];
+    }
+   }
+}
