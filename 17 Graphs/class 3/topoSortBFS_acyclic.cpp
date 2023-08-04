2b812e6d1e800e0b5698165dfc55297d7183a3c9
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-05 04:09:29 +0530

    Update topoSortBFS_acyclic.cpp

--- 17 Graphs/class 3/topoSortBFS_acyclic.cpp
+++ 17 Graphs/class 3/topoSortBFS_acyclic.cpp
@@ -44,14 +44,30 @@ bool checkcyclic(int src,unordered_map<int,bool>& visted,unordered_map<int,bool>
     dfsvisted[src]=false;
     return false;
 }
-void topoSortDFS(int src,unordered_map<int,bool> & visted,stack<int>& st){
-    visted[src]=true;
-    for(auto i:adj[src]){
-        if(!visted[i]){
-            topoSortDFS(i,visted,st);
+void topoSortBFS(int n,vector<int>& ans){
+    unordered_map<int,int> indegree;
+    queue<int> q;
+    for(auto i:adj){
+        for(auto nbr:i.second)
+            indegree[nbr]++;
+    }
+    
+    for(auto i:indegree){
+        if(i.second==0)
+            q.push(i.first);
+    }
+
+    while(!q.empty()){
+        int fnode=q.front();
+        q.pop();
+        ans.push_back(fnode);
+        for(auto i : adj[fnode]){
+            if(indegree[i]>0)
+                indegree[i]--;
+                else
+                ans.push_back(i);
         }
     }
-    st.push(src);
 }
 };
 int main(){
@@ -63,15 +79,8 @@ int main(){
     g.addedge(3,4,1);
     g.addedge(4,0,1);
     g.printadj();
-    bool ans=false;
-    unordered_map<int, bool> visited;
-    stack<int> st;
-    for(int i=0;i<n;i++){
-        if(!visited[i])
-            g.topoSortDFS(i,visited,st);
-    }
-    while(!st.empty()){
-        cout<<st.top()<<" , ";
-        st.pop();
-    }
+    vector<int> ans;
+     g.topoSortBFS(n,ans);
+    for(auto i : ans)
+        cout<<i<<" , ";
 }
