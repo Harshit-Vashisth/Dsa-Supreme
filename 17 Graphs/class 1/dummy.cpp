468faf594c16e69ae8f31847bac3be029ff29ca3
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-09 03:08:17 +0530

    h

--- 17 Graphs/class 1/dummy.cpp
+++ 17 Graphs/class 1/dummy.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>n>>e;
     int u,v;
     vector<vector<int>> adj(n,vector<int> (n,0));
-    for(int i=0;i<n;i++){
+    for(int i=0;i<e;i++){
         cin>>u>>v;
         adj[u][v]=1;
         }
