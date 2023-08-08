5e58c9a7465f63378ed30ccb797098fb14454ede
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-09 03:47:21 +0530

    by harshit

--- 17 Graphs/class 1/dummy.cpp
+++ 17 Graphs/class 1/dummy.cpp
@@ -16,6 +16,31 @@ class Graph{
         }
         cout<<endl;
     }
+    void traversebfs(int src){
+        queue<int> q;
+        unordered_map<int,bool> visited;
+        q.push(src);
+        visited[src]=true;
+        while(!q.empty()){
+            int fnode=q.front();
+            q.pop();
+            cout<<fnode;
+            for(auto i : adj[fnode]){
+                if(!visited[i])
+                    q.push(i);
+                    visited[i]=true;
+            }
+        }
+    }
+    void dfs(int src,unordered_map<int,bool>& visited){
+        cout<<"src"<<", ";
+        visited[src]=true;
+        for(auto nbr:adj[src])
+        {
+            if(!visited[nbr])
+                dfs(src,visited);
+        }
+    }
 };
 int main(){
     Graph g;
@@ -26,6 +51,9 @@ int main(){
     g.addadj(4,5,1);
     g.addadj(5,6,1);
     g.printadj();
+    g.traversebfs(0);
+    unordered_map<int,bool> visited;
+    g.dfs(0,visited);
     // int n,e;
     // cout<<"Enter the number of nodes and edges "<<endl;
     // cin>>n>>e;
