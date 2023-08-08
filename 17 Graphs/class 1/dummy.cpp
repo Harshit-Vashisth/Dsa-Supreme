53129464486150292d586583aa9c5c2a0cd80c51
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-09 03:01:41 +0530

    Create dummy.cpp

---
+++ 17 Graphs/class 1/dummy.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,e;
+    cout<<"Enter the number of nodes and edges "<<endl;
+    cin>>n>>e;
+    int u,v;
+    vector<vector<int>> adj(n,vector<int> (n,0));
+    for(int i=0;i<n;i++){
+        cin>>u>>v;
+        adj[u][v]=1;
+        }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
