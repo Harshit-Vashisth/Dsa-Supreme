49ea4d77e5b1e1b445d62175649460b85a85e5d9
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-09 03:27:05 +0530

    by harshit

--- 17 Graphs/class 1/dummy.cpp
+++ 17 Graphs/class 1/dummy.cpp
@@ -1,19 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int n,e;
-    cout<<"Enter the number of nodes and edges "<<endl;
-    cin>>n>>e;
-    int u,v;
-    vector<vector<int>> adj(n,vector<int> (n,0));
-    for(int i=0;i<e;i++){
-        cin>>u>>v;
-        adj[u][v]=1;
-        }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<adj[i][j]<<" ";
+class Graph{
+    public:
+    unordered_map<int,list<int>> adj;
+    void addadj(int u,int v,int direct){
+        adj[u].push_back(v);
+        if(direct==0)
+            adj[v].push_back(u);
+    }
+    void printadj(){
+        for(auto i:adj){
+            cout<<i.first<<"->";
+            for(auto nbr:i.second)
+                cout<<nbr<<", ";
         }
         cout<<endl;
     }
+};
+int main(){
+    Graph g;
+    g.addadj(0,1,1);
+    g.addadj(1,2,1);
+    g.addadj(2,3,1);
+    g.addadj(3,4,1);
+    g.addadj(4,5,1);
+    g.addadj(5,6,1);
+    g.printadj();
+    // int n,e;
+    // cout<<"Enter the number of nodes and edges "<<endl;
+    // cin>>n>>e;
+    // int u,v;
+    // vector<vector<int>> adj(n,vector<int> (n,0));
+    // for(int i=0;i<e;i++){
+    //     cin>>u>>v;
+    //     adj[u][v]=1;
+    //     }
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         cout<<adj[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
 }
