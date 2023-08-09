2cfaa0ed2eedbf686a9b287e1b0b37af70c1a467
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-09 22:59:42 +0530

    Update undirected_BFS.cpp

--- 17 Graphs/class 2/undirected_BFS.cpp
+++ 17 Graphs/class 2/undirected_BFS.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class Graph{
     public:
     unordered_map<int,list<int>> adj;
-    void addadj(int u, int v ,int direct){
+    void addedge(int u, int v ,int direct){
         adj[u].push_back(v);
         if(direct==0)
         adj[v].push_back(u);
@@ -18,3 +18,13 @@ class Graph{
         }
     }
 };
+int main(){
+    Graph g;
+    int n=5;
+    g.addedge(0,1,1);
+    g.addedge(1,2,1);
+    g.addedge(2,3,1);
+    g.addedge(3,4,1);
+    g.addedge(4,0,1);
+    g.print();
+}
