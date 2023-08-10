69628d938fff4375854e8bd82f6bfe429e1e5825
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-11 02:59:28 +0530

    by harshit

--- 17 Graphs/class 3/dummy.cpp
+++ 17 Graphs/class 3/dummy.cpp
@@ -16,13 +16,32 @@ class Graph{
             cout<<endl;
         }
     }
+    void toposort(int src,unordered_map<int,bool> & visited,stack<int>& ans){
+        visited[src]=true;
+        for(auto nbr:adj[src]){
+            if(!visited[nbr])
+                toposort(nbr,visited,ans);
+        }
+        ans.push(src);
+    }
 };
 int main(){
     Graph g;
+    int n=5;
     g.addedge(0,1,1);
     g.addedge(1,2,1);
     g.addedge(2,3,1);
     g.addedge(3,4,1);
     g.addedge(4,5,1);
     g.print();
+    unordered_map<int,bool> visited;
+    stack<int> ans;
+    for(int i=0;i<n;i++){
+        if(!visited[i])
+        g.toposort(i,visited,ans);
+    }
+    while(!ans.empty()){
+        cout<<ans.top();
+        ans.pop();
+    }
 }
