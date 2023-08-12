aebded2e317eabe8d91b7a17b4a648e77426fda3
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-13 02:25:45 +0530

    by harshit

--- 17 Graphs/class 4/shortestPATHDFS.cpp
+++ 17 Graphs/class 4/shortestPATHDFS.cpp
@@ -1,38 +1,140 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include<unordered_map>
+#include<list>
+#include<queue>
+#include<stack>
+#include<algorithm>
+#include<set>
+#include<limits.h>
 using namespace std;
-class Graph{
-    public:
-    unordered_map<int,list<int>> adj;
-    void addedge(int u,int v,int direct){
-        adj[u].push_back(v);
-        if(direct==0)
-            adj[v].push_back(u);
-    }
-    void print(){
-        for(auto i: adj){
-            cout<<i.first<<"-> ";
-            for(auto nbr:i.second){
-                cout<<nbr<<", ";
-            }
-            cout<<endl;
-        }
-    }
-    void topoSort(){
 
-    }
-    void shortestpathDFS(int n,stack<int>& st){
-        
-    }
+
+class graph {
+	public:
+	unordered_map<int, list<pair<int,int> > > adjList;
+
+	void addEdge(int u, int v, int wt, bool direction) {
+		//direction = 1 -> undirected graph
+		//direction => 0 -> directed graph;
+		adjList[u].push_back({v,wt});
+		if(direction == 1) {
+			adjList[v].push_back({u,wt});
+		}
+	}
+
+	void printAdjList() {
+		for(auto i: adjList) {
+			cout << i.first <<"-> ";
+			for(auto j: i.second) {
+				cout << "(" << j.first<<", "<<j.second<<"), ";
+			}
+			cout << endl;
+		}
+	}
+
+	void shortestPathBfs(int src, int dest) {
+		queue<int> q;
+		unordered_map<int,bool> visited;
+		unordered_map<int,int> parent;
+
+		//inital steps for src
+		q.push(src);
+		visited[src] = 1;
+		parent[src] = -1;
+
+		while(!q.empty()) {
+			int fNode = q.front();
+			q.pop();
+
+			for(auto nbr: adjList[fNode]) {
+				if(!visited[nbr.first]) {
+					q.push(nbr.first);
+					visited[nbr.first] = 1;
+					parent[nbr.first] = fNode;
+				}
+			}
+		}
+
+		//store path in ans, after traversing in the parent array
+		vector<int> ans;
+		int node = dest;
+		while(node != -1) {
+			ans.push_back(node);
+			node = parent[node];
+		}
+
+		reverse(ans.begin(), ans.end());
+
+		cout << "printing ANs: " << endl;
+		for(auto i: ans) {
+			cout << i<<", ";
+		}
+		cout << endl;
+		
+	}
+
+
+	void topoSortDfs(int src, unordered_map<int, bool>& visited, stack<int>& ans) {
+		
+		visited[src] = true;
+
+		for(auto neighbour: adjList[src]) {
+			if(!visited[neighbour.first] ) {
+				topoSortDfs(neighbour.first, visited, ans);
+			}
+		}
+
+		//while returning, store the node in stack
+		cout << "Pushing " << src << endl;
+		ans.push(src);
+	}
+
+ 	void shortestpathDfs(stack<int>& st, int n) {
+		vector<int> dist(n, INT_MAX);
+
+		int src = st.top();
+		st.pop();
+		dist[src] = 0;
+        for(auto nbr: adjList[0]){
+            if(dist[0]+nbr.second<dist[nbr.first])
+                dist[nbr.first]=dist[0]+nbr.second;
+        }
+        while(!st.empty()){
+            int topele=st.top();
+            st.pop();
+            if(dist[topele]!=INT_MAX){
+            for(auto nbr:adjList[topele]){
+                if(dist[topele]+nbr.second<dist[nbr.first])
+                    dist[nbr.first]=dist[topele]+nbr.second;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            cout<<dist[i]<<"  ";
+        }
+	}
+
+	
 };
-int main(){
-    Graph g;
-    g.addedge(0,1,1);
-    g.addedge(1,2,1);
-    g.addedge(2,3,1);
-    g.addedge(2,4,1);
-    g.addedge(3,5,1);
-    g.addedge(4,6,1);
-    g.addedge(5,7,1);
-    g.addedge(6,7,1);
-    g.print();
+
+int main() {
+	graph g;
+
+	g.addEdge(6,3,2,1);
+	g.addEdge(6,1,14,1);
+	g.addEdge(3,1,9,1);
+	g.addEdge(3,2,10,1);
+	g.addEdge(1,2,7,1);
+	g.addEdge(2,4,15,1);
+	g.addEdge(4,3,11,1);
+
+	g.addEdge(6,5,9,1);
+	g.addEdge(4,5,6,1);
+ 
+    stack<int> st;
+    unordered_map<int,bool> visited;
+    g.topoSortDfs(1,visited,st);
+    g.shortestpathDfs(st,6);
+
+	return 0;
 }
