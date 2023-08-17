4325782501e5d2aed0c020701ecc05675239cb23
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-18 00:31:12 +0530

    by harshit

--- 17 Graphs/class 1/dummy.cpp
+++ 17 Graphs/class 1/dummy.cpp
@@ -5,7 +5,7 @@ class Graph{
     unordered_map<int,list<int>> adj;
     void addedge(int u,int v,int direction){
         adj[u].push_back(v);
-        if(direction==0)
+        if(direction==1)
             adj[v].push_back(u);
     }
     void printedge(){
@@ -17,15 +17,31 @@ class Graph{
             cout<<endl;
         }
     }
+    void taraversalbfs(int src){
+        queue<int> q;
+        unordered_map<int,bool> visited;
+        q.push(src);
+        visited[src]=true;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            cout<<node<<", ";
+            for(auto nbr: adj[node]){
+                if(!visited[nbr])
+                    q.push(nbr);
+                    visited[nbr]=true;
+            }
+        }
+    }
 };
 int main(){
     Graph g;
-    g.addedge(0,1,1);
-    g.addedge(1,3,1);
-    g.addedge(2,1,1);
-    g.addedge(3,2,1);
-    g.addedge(4,3,1);
-    g.addedge(4,1,1);
-    g.addedge(3,1,1);
+    g.addedge(0,1,0);
+    g.addedge(1,2,0);
+    g.addedge(2,3,0);
+    g.addedge(3,1,0);
+    g.addedge(3,4,0);
+    g.addedge(4,3,0);
     g.printedge();
+    g.taraversalbfs(0);
 }
