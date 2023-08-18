1fe9be75d7d3faeee5f21a34f4d9557de27e9910
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-19 01:50:12 +0530

    Update dummy.cpp

--- 17 Graphs/class 3/dummy.cpp
+++ 17 Graphs/class 3/dummy.cpp
@@ -2,65 +2,67 @@
 using namespace std;
 class Graph{
     public:
-    unordered_map<int,list<int>> adj;
-    void addedge(int u, int v ,int direct){
-        adj[u].push_back(v);
-        if(direct==0)
+   unordered_map<int, list<int>> adj;
+void addedge(int u,int v,bool direction){
+    //direction 0 undirected
+    //direction 1 directed
+    adj[u].push_back(v);
+    if(direction==0)
         adj[v].push_back(u);
-    }
-    void print(){
-        for(auto i: adj){
-            cout<<i.first<<"-> ";
-            for( auto nbr: i.second){
-                cout<<nbr<<", ";
-            }
-            cout<<endl;
+}
+void printadj(){
+    for(auto i:adj){
+        cout<<i.first<<"->";
+        for(auto neigh:i.second){
+            cout<<neigh<<" ,";
         }
+        cout<<endl;
     }
-    bool cycledetect(int src,unordered_map<int,bool>& visited){
-        queue<int> q;
-        
-        unordered_map<int,int> parent;
-        q.push(src);
-        visited[src]=true;
-        parent[src]=-1;
-        while(!q.empty()){
-            int fNode=q.front();
-            q.pop();
-            for(auto nbr: adj[fNode]){
-                if(!visited[nbr]){
-                    visited[nbr]=true;
-                    parent[nbr]=fNode;
-                    q.push(nbr);
-                }
-                else if(nbr!=parent[fNode]){
-                    return true;
-                }
+}
+void dfs(int src,unordered_map<int, bool>& visited){
+    cout<<src<<" ";
+    visited[src]=true;
+        for(auto i:adj[src]){
+            if(!visited[i]){
+                dfs(i,visited);
             }
         }
-        return false;
     }
+bool checkcyclic(int src,unordered_map<int,bool>& visted,int parent){
+      visted[src]=true;
+
+    for(auto nbr: adj[src])
+    {
+    if(!visted[nbr]){
+        bool check=checkcyclic(nbr,visted,src);
+        if(check==true)
+            return true;
+    }
+    else if( nbr!= parent)
+        return true;
+    }
+    return false;
+}
 };
 int main(){
     Graph g;
-    int n=5;
-    g.addedge(0,1,0);
-    g.addedge(1,2,0);
-    g.addedge(2,3,0);
-    g.addedge(3,4,0);
-    g.addedge(4,2,0);
-    g.print();
+    int n=4;
+    g.addedge(0,1,1);
+    g.addedge(1,2,1);
+    g.addedge(2,3,1);
+    g.addedge(3,4,1);
+    // g.addedge(4,0,1);
+    g.printadj();
     bool ans=false;
-    unordered_map<int,bool> visited;
-    for(int i=0;i<n;i++){
-        if(!visited[i]){
-            ans=g.cycledetect(i,visited);
-            if(ans==true)
-                break;
-        }
+    unordered_map<int, bool> visited;
+    for(int i=0;i<5;i++){
+        if(!visited[i])
+            ans=g.checkcyclic(i,visited,-1);
+        if(ans==true)
+            break;
     }
     if(ans==true)
-        cout<<"Cycle is prsent"<<endl;
+        cout<<"Cycle is present"<<endl;
     else
-        cout<<"Cycle is absent"<<endl;
+        cout<<"Cycle is not present"<<endl;
 }
