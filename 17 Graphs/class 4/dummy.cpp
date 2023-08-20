9916ae5b0c0d3ef0f2c66f468f69d55a9dbf73d3
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-20 18:03:56 +0530

    by harshit

--- 17 Graphs/class 4/dummy.cpp
+++ 17 Graphs/class 4/dummy.cpp
@@ -6,10 +6,10 @@ class Graph{
     unordered_map<int,list<pair<int,int>>> adj;
     void addedge(int u, int v,int wt,int direct){
         adj[u].push_back({v,wt});
-        if(direct==1)
+        if(direct==0)
            adj[v].push_back({u,wt}); 
     }
-    void printAdjList() {
+    void print() {
 		for(auto i: adj) {
 			cout << i.first <<"-> ";
 			for(auto j: i.second) {
@@ -18,17 +18,47 @@ class Graph{
 			cout << endl;
 		}
 	}
+    void shortestpathBFS(int src,int dest){
+        queue<int> q;
+        unordered_map<int,bool> visited;
+        unordered_map<int,int> parent;
+        q.push(src);
+        visited[src]=true;
+        parent[src]=-1;
+        while(!q.empty()){
+            int fNode=q.front();
+            q.pop();
+            for(auto nbr: adj[fNode]){
+                if(!visited[nbr.first]){
+                    q.push(nbr.first);
+                    visited[nbr.first]=true;
+                    parent[nbr.first]=fNode;
+                }
+            }
+        }
+        stack<int> ans;
+        int node=dest;
+        while(node!=-1){
+            ans.push(node);
+            node=parent[node];
+        }
+        cout<<"printing answer";
+        while(!ans.empty()){
+            cout<<ans.top();
+            ans.pop();
+            }
+    }
 };
 int main(){
     Graph g;
-    g.addedge(0,1,1);
-    g.addedge(1,2,1);
-    g.addedge(2,3,1);
-    g.addedge(2,4,1);
-    g.addedge(3,5,1);
-    g.addedge(4,6,1);
-    g.addedge(5,7,1);
-    g.addedge(6,7,1);
+    g.addedge(1,2,1,1);
+    g.addedge(2,3,1,1);
+    g.addedge(0,1,1,1);
+    g.addedge(2,4,1,1);
+    g.addedge(3,5,1,1);
+    g.addedge(4,6,1,1);
+    g.addedge(5,7,1,1);
+    g.addedge(6,7,1,1);
     g.print();
-    g.shortestpathBFS(0,6);
+    g.shortestpathBFS(0,7);
 }
