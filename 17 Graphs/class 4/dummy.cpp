b458c7fbb514f434d030566814588388ed7ddfc8
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-21 01:57:14 +0530

    by harshit

--- 17 Graphs/class 4/dummy.cpp
+++ 17 Graphs/class 4/dummy.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Graph{
     public : 
     unordered_map<int,list<pair<int,int>>> adj;
-    void addedge(int u, int v,int wt,int direct){
+    void addEdge(int u, int v,int wt,int direct){
         adj[u].push_back({v,wt});
         if(direct==0)
            adj[v].push_back({u,wt}); 
@@ -48,17 +48,85 @@ class Graph{
             ans.pop();
             }
     }
+    void topoSortDfs(int src, unordered_map<int, bool>& visited, stack<int>& topo) {
+		
+		visited[src] = true;
+
+		for(auto neighbour: adj[src]) {
+			if(!visited[neighbour.first] ) {
+				topoSortDfs(neighbour.first, visited, topo);
+			}
+		}
+
+		//while returning, store the node in stack
+		cout << "Pushing " << src << endl;
+		topo.push(src);
+	}
+
+    void shortestPathDFS(stack<int>& topo,int dest,int n){
+        vector<int> dist(n,INT_MAX);
+        int src=topo.top(); topo.pop();
+        dist[src]=0;
+        for(auto nbr: adj[0]){
+            if(dist[0]+nbr.second<dist[nbr.first])
+                dist[nbr.first]=dist[0]+nbr.second;
+        }
+        while(!topo.empty()){
+            int topelement=topo.top();
+            topo.pop();
+            if(dist[topelement]!=INT_MAX){
+                for(auto nbr:adj[topelement]){
+                    if(dist[topelement]+nbr.second<dist[nbr.first])
+                        dist[nbr.first]=dist[topelement]+nbr.second;
+                }
+            }
+        }
+        cout<<"printing";
+        for(auto i : dist){
+            cout<<i<<' ';
+        }
+    }
+    void dijkastra(int src,int n){
+        set<pair<int,int>> st;
+        vector<int> dist(n,INT_MAX);
+        dist[src]=0;
+        st.insert(make_pair(0,src));
+        while(!st.empty()){
+            auto topElement=*(st.begin());
+            int nodeDis= topElement.first;
+            int node=topElement.second;
+            st.erase(st.begin());
+            for(auto nbr: adj[node]){
+                if(nodeDis+nbr.second<dist[nbr.first]){
+                    auto result=st.find(make_pair(dist[nbr.first],nbr.first));
+                    if(result!=st.end())
+                        st.erase(result);
+                    dist[nbr.first]=nodeDis+nbr.second;
+                    st.insert(make_pair(dist[nbr.first],nbr.first));
+                }
+            }
+        }
+        for(auto i:dist){
+            cout<<i<<" ";
+        }
+    }
 };
 int main(){
     Graph g;
-    g.addedge(1,2,1,1);
-    g.addedge(2,3,1,1);
-    g.addedge(0,1,1,1);
-    g.addedge(2,4,1,1);
-    g.addedge(3,5,1,1);
-    g.addedge(4,6,1,1);
-    g.addedge(5,7,1,1);
-    g.addedge(6,7,1,1);
-    g.print();
-    g.shortestpathBFS(0,7);
+   
+	g.addEdge(6,3,2,1);
+	g.addEdge(6,1,14,1);
+	g.addEdge(3,1,9,1);
+	g.addEdge(3,2,10,1);
+	g.addEdge(1,2,7,1);
+	g.addEdge(2,4,15,1);
+	g.addEdge(4,3,11,1);
+
+	g.addEdge(6,5,9,1);
+	g.addEdge(4,5,6,1);
+ 
+	g.print();
+    
+    g.dijkastra(6,7);
+	return 0;
 }
--- 17 Graphs/class 4/tempCodeRunnerFile.cpp
+++ 17 Graphs/class 4/tempCodeRunnerFile.cpp
@@ -1,1 +1,4 @@
-dist[topele]+nbr.second
+stack<int> topo;
+    // unordered_map<int , bool> visted;
+    // g.topoSortDfs(0,visted,topo);
+    // g.shortestPathDFS(topo,4,5);
