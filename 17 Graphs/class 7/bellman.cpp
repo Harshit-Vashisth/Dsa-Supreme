c2d11d55e2c88e040a581d04261a7a6ca44e6176
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-25 21:21:41 +0530

    Update bellman.cpp

---
+++ 17 Graphs/class 7/bellman.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Graph{
+    public:
+    unordered_map<int,list<pair<int,int>>> adj;
+    void addEdges(int u ,int v,int wt,direct){
+        // 1 ->undirected 
+        // 0->directed
+    }
+};
+int main(){
+    Graph g;
+
+}
