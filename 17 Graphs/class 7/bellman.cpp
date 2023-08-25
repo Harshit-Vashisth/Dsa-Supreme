a0d80af1dfdc99eb02dfca6256b048f59978e097
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-25 21:30:46 +0530

    by harshit

--- 17 Graphs/class 7/bellman.cpp
+++ 17 Graphs/class 7/bellman.cpp
@@ -3,10 +3,22 @@ using namespace std;
 class Graph{
     public:
     unordered_map<int,list<pair<int,int>>> adj;
-    void addEdges(int u ,int v,int wt,int direct){
+    void addEdges(int u ,int v,int wt,int  direct){
         // 1 ->undirected 
         // 0->directed
-
+        adj[u].push_back({v,wt});
+        if(direct==1)
+        adj[v].push_back({u,wt});
+        
+    }
+    void printadj(){
+        for(auto i:adj){
+            cout<<i.first<<"->";
+            for(auto neigh:i.second){
+                cout<<neigh<<" ,";
+            }
+            cout<<endl;
+        }
     }
 };
 int main(){
