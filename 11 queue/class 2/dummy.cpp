f23a636939de14e4d6b96d009c1ddb0ee2feac0e
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-09-14 01:23:53 +0530

    by harshit

--- 11 queue/class 2/dummy.cpp
+++ 11 queue/class 2/dummy.cpp
@@ -14,6 +14,28 @@ void reverse(queue<int>& q){
     reverse(q);
     q.push(num);
 }
+void reversek(queue<int>& q, int k){
+    int n=q.size();
+    if(q.size()<k)
+        return;
+    int count=0;
+    stack<int> st;
+    while(count<k){
+        st.push(q.front());
+        q.pop();
+        count++;
+    }
+    while(!st.empty()){
+        q.push(st.top());
+        st.pop();
+    }
+    while(count<n){
+        int top=q.front();
+        q.pop();
+        q.push(top);
+        count++;
+    }
+}
 int main(){
     queue<int> q;
     q.push(3);
@@ -22,7 +44,11 @@ int main(){
     q.push(2);
     q.push(8);
     print(q);
-    reverse(q);
+    // reverse(q);
+    // cout<<endl;
+    // print(q);
+    int k=3;
+    reversek(q,k);
     cout<<endl;
     print(q);
 }
